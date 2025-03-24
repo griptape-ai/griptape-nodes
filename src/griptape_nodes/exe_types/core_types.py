4aef07572b08dc5c91d42b52227c8d501fc3e8cf
Collin Dutter <collin@griptape.ai>
2025-03-24 15:31:07 -0700

    refactor: remove validations parameter (#48)
    Closes https://github.com/griptape-ai/griptape-nodes/issues/8

--- src/griptape_nodes/exe_types/core_types.py
+++ src/griptape_nodes/exe_types/core_types.py
@@ -129,7 +129,6 @@ class Parameter:
     tooltip_as_output: str | None = None
     settable: bool = True
     user_defined: bool = False
-    validations: bool = True  # This exists to accelerate early prototyping and node work.  We shouldn't be refactoring this piecemeal, but come back to it with a larger clearer set of needs
     allowed_modes: set = field(
         default_factory=lambda: {
             ParameterMode.OUTPUT,
@@ -143,10 +142,6 @@ class Parameter:
     prev: Parameter | None = None
 
     def is_type_allowed(self, type_as_str: str) -> bool:
-        # Skip validation if validations is False
-        if not self.validations:
-            return True
-
         # Original code continues here...
         # Can't just do a string compare as we'll whiff on things like "list" not matching "List"
         test_type = TypeValidator.convert_to_type(type_as_str)
@@ -161,9 +156,6 @@ class Parameter:
         return False
 
     def is_value_allowed(self, value: Any) -> bool:
-        if not self.validations:  # TEMP FOR DEV - JO
-            return True  # TEMP FOR DEV - JO
-
         for allowed_type_str in self.allowed_types:
             if TypeValidator.is_instance(value, allowed_type_str):
                 return True
@@ -174,7 +166,7 @@ class Parameter:
         return False
 
     def set_default_value(self, value: Any) -> None:
-        if not self.validations or self.is_value_allowed(value):
+        if self.is_value_allowed(value):
             self.default_value = value
         else:
             errormsg = "Type does not match allowed value types"
