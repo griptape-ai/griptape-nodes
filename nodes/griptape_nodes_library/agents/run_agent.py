5291c4b8ad63d77a0381ae4730ff9ff3e7a3761e
Jason Schleifer <jason.schleifer@gmail.com>
2025-03-28 11:29:36 +1300

    update the name of the run_agent

---
+++ nodes/griptape_nodes_library/agents/run_agent.py
@@ -0,0 +1,60 @@
+from griptape.drivers.prompt.griptape_cloud import (
+    GriptapeCloudPromptDriver as GtGriptapeCloudPromptDriver,
+)
+from griptape.structures import Agent as gtAgent
+from griptape.utils import Stream
+
+from nodes.griptape_nodes_library.agents.create_agent import Agent
+
+DEFAULT_MODEL = "gpt-4o"
+API_KEY_ENV_VAR = "GT_CLOUD_API_KEY"
+SERVICE = "Griptape"
+
+
+class RunAgent(Agent):
+    def __init__(self, **kwargs) -> None:
+        super().__init__(**kwargs)
+
+        # Remove unused inputs
+        param = self.get_parameter_by_name("rulesets")
+        if param:
+            self.remove_parameter(param)
+        param = self.get_parameter_by_name("tool_list")
+        if param:
+            self.remove_parameter(param)
+        param = self.get_parameter_by_name("prompt_driver")
+        if param:
+            self.remove_parameter(param)
+
+    def process(self) -> None:
+        # Get input values
+        params = self.parameter_values
+        agent_dict = params.get("agent", None)
+
+        if not agent_dict:
+            prompt_driver = GtGriptapeCloudPromptDriver(
+                model="gpt-4o",
+                api_key=self.getkey(value=f"env.{SERVICE}.{API_KEY_ENV_VAR}"),
+                stream=True,
+            )
+            agent = gtAgent(prompt_driver=prompt_driver)
+        else:
+            agent = gtAgent().from_dict(agent_dict)
+
+        prompt = params.get("prompt", None)
+        if prompt:
+            full_output = ""
+            # Check and see if the prompt driver is a stream driver
+            if self.is_stream(agent):
+                # Run the agent
+                for artifact in Stream(agent).run(prompt):
+                    full_output += artifact.value
+            else:
+                # Run the agent
+                result = agent.run(prompt)
+                full_output = result.output.value
+            self.parameter_output_values["agent_response"] = full_output
+        else:
+            self.parameter_output_values["agent_response"] = "Agent Created"
+
+        self.parameter_output_values["agent"] = agent.to_dict()
