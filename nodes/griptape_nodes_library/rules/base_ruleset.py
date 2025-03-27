0c39d72887fd2e03162b657e6f322c390885c9c0
Jason Schleifer <jason.schleifer@gmail.com>
2025-03-28 10:58:43 +1300

    updated ruleset

--- nodes/griptape_nodes_library/rules/base_ruleset.py
+++ nodes/griptape_nodes_library/rules/base_ruleset.py
@@ -1,10 +1,11 @@
-from griptape.rules import Rule, Ruleset
+from griptape.rules import Rule as gnRule
+from griptape.rules import Ruleset as gnRuleset
 
 from griptape_nodes.exe_types.core_types import Parameter, ParameterMode, ParameterUIOptions
 from griptape_nodes.exe_types.node_types import DataNode
 
 
-class gnRuleSet(DataNode):
+class Ruleset(DataNode):
     def __init__(self, **kwargs) -> None:
         super().__init__(**kwargs)
 
@@ -46,11 +47,11 @@ class gnRuleSet(DataNode):
         params = self.parameter_values
         name = params.get("name", "Behavior")
         raw_rules = params.get("rules", "")
-        sep_rules = [Rule(rule) for rule in raw_rules.split("\n")]
-        ruleset = Ruleset(name=name, rules=sep_rules)  # was in [], but made type validation bad for austin
+        sep_rules = [gnRule(rule) for rule in raw_rules.split("\n")]
+        ruleset = gnRuleset(name=name, rules=sep_rules)  # was in [], but made type validation bad for austin
 
         self.parameter_output_values["ruleset"] = ruleset
 
 
 if __name__ == "__main__":
-    gnRuleSet(name="gnRuleSet_1").process()
+    Ruleset(name="Ruleset_1").process()
