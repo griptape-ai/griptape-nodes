a568c272900d07f5d341cb2ba3b6f1b8b09fce78
cjkindel <cjkindel@users.noreply.github.com>
2025-03-27 17:07:05 -0700

    Implement log handler events

--- src/griptape_nodes/retained_mode/managers/log_manager.py
+++ src/griptape_nodes/retained_mode/managers/log_manager.py
@@ -2,6 +2,14 @@ import logging
 
 from rich.logging import RichHandler
 
+from griptape.events import EventBus
+from griptape_nodes.retained_mode.events.base_events import AppEvent
+from griptape_nodes.retained_mode.events.logger_events import LogHandlerEvent
+
+
+class EventLogHandler(logging.Handler):
+    def emit(self, record):
+        EventBus.publish_event(AppEvent(payload=LogHandlerEvent(message=record.getMessage(), levelname=record.levelname, created=record.created)))
 
 class LogManager:
     def __init__(self) -> None:
@@ -10,6 +18,7 @@ class LogManager:
 
         if not root_logger.hasHandlers():
             root_logger.addHandler(RichHandler(show_time=True, show_path=False, markup=True))
+            root_logger.addHandler(EventLogHandler())
 
     def get_logger(self) -> logging.Logger:
         logger = logging.getLogger()
