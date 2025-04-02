688d17e24563b88c130660ad00df702e788fdf45
Collin Dutter <collin@griptape.ai>
2025-04-01 18:27:04 -0700

    refactor: initialize config in main rather than install scripts
    Minimizes ugly shell scripts and future proofs us for other installation
    methods like homebrew

--- src/griptape_nodes/__init__.py
+++ src/griptape_nodes/__init__.py
@@ -6,10 +6,17 @@ import subprocess
 import sys
 from pathlib import Path
 
+from dotenv import get_key, set_key
+from rich.prompt import Prompt
+from xdg_base_dirs import xdg_config_home
+
 from griptape_nodes.api.app import main as api_main
 from griptape_nodes.retained_mode.managers.config_manager import ConfigManager
 
 INSTALL_SCRIPT = "https://raw.githubusercontent.com/griptape-ai/griptape-nodes/refs/heads/main/install.sh"
+CONFIG_DIR = xdg_config_home() / "griptape_nodes"
+ENV_FILE = CONFIG_DIR / ".env"
+CONFIG_FILE = CONFIG_DIR / "griptape_nodes_config.json"
 
 
 def main() -> None:
@@ -17,6 +24,8 @@ def main() -> None:
     # Without this, packages like `nodes` don't properly import.
     # Long term solution could be to make `nodes` a proper src-layout package
     # but current engine relies on importing files rather than packages.
+    _init_config()
+    _init_api_key()
     sys.path.append(str(Path.cwd()))
 
     args = _get_args()
@@ -29,6 +38,37 @@ def _get_args() -> argparse.Namespace:
         "command", help="Command to run", nargs="?", choices=["engine", "config", "update"], default="engine"
     )
     return parser.parse_args()
+
+
+def _init_config() -> None:
+    """Initializes the config directory if it doesn't exist."""
+    config_dir = xdg_config_home() / "griptape_nodes"
+    if not config_dir.exists():
+        config_dir.mkdir(parents=True, exist_ok=True)
+
+    files_to_create = [
+        ENV_FILE,
+        CONFIG_FILE,
+    ]
+
+    for file_name in files_to_create:
+        file_path = config_dir / file_name
+        if not file_path.exists():
+            file_path.touch()
+
+
+def _init_api_key() -> None:
+    """Prompts the user for their GT_CLOUD_API_KEY and stores it in config directory."""
+    api_key = get_key(ENV_FILE, "GT_CLOUD_API_KEY")
+
+    if not api_key:
+        while not api_key:
+            api_key = Prompt.ask(
+                "Please enter your API key to continue",
+                default=None,
+                show_default=False,
+            )
+        set_key(ENV_FILE, "GT_CLOUD_API_KEY", api_key)
 
 
 def _process_args(args: argparse.Namespace) -> None:
