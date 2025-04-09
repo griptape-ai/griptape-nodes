7362b6104a7dbc3928817a2a6ee0fda497ba3763
Collin Dutter <collin@griptape.ai>
2025-04-09 10:27:27 -0700

    revert: don't use instance fields for registries
    https://github.com/griptape-ai/griptape-nodes/pull/244#discussion_r2033601642

--- src/griptape_nodes/traits/trait_registry.py
+++ src/griptape_nodes/traits/trait_registry.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, ClassVar
 
 from griptape.mixins.singleton_mixin import SingletonMixin
 
@@ -12,11 +12,7 @@ if TYPE_CHECKING:
 class TraitRegistry(SingletonMixin):
     # I'm going to create a dictionary that stores all of the created traits we have so far?
     # Traits will be associated with certain key words
-    key_to_trait: dict[str, list[Trait.__class__]]
-
-    def __init__(self) -> None:
-        super().__init__()
-        self.key_to_trait = {}
+    key_to_trait: ClassVar[dict[str, list[Trait.__class__]]] = {}
 
     @classmethod
     def create_traits(cls, key_word: str) -> list[Trait] | None:
