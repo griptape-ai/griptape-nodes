d22c12b66ef768b865f142d808a1a9371d705435
James Clarendon <james.clarendon@gmail.com>
2025-04-09 22:02:42 -0700

    Updated the event handler to echo the exception when event serialization fails.

--- src/griptape_nodes/app/app.py
+++ src/griptape_nodes/app/app.py
@@ -146,7 +146,7 @@ def _listen_for_api_events() -> None:
                                 else:
                                     __process_api_event(event)
                             except Exception:
-                                logger.warning("Error processing event, skipping.")
+                                logger.exception("Error processing event, skipping.")
 
         except Exception:
             logger.error("Error while listening for events. Retrying in 2 seconds.")  # noqa: TRY400
