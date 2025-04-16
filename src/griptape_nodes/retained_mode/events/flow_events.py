fb0df99baf6d4d65b2f12869d711b8debb5b5a59
Kate Forsberg <77945737+kateforsberg13@users.noreply.github.com>
2025-04-16 10:26:08 -0700

    Adding GetTopLevelFlowRequest (#435)
    * adding new event for andrew
    
    * removed error log
    
    * get top level flow request
    
    * got rid of failure
    
    * changed to debu

--- src/griptape_nodes/retained_mode/events/flow_events.py
+++ src/griptape_nodes/retained_mode/events/flow_events.py
@@ -81,3 +81,15 @@ class ListFlowsInFlowResultSuccess(ResultPayloadSuccess):
 @PayloadRegistry.register
 class ListFlowsInFlowResultFailure(ResultPayloadFailure):
     pass
+
+
+@dataclass
+@PayloadRegistry.register
+class GetTopLevelFlowRequest(RequestPayload):
+    pass
+
+
+@dataclass
+@PayloadRegistry.register
+class GetTopLevelFlowResultSuccess(ResultPayloadSuccess):
+    flow_name: str | None
