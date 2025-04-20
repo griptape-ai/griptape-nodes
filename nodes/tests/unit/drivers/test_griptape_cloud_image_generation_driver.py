d9fc3ef26023e217d126e9e08b5ea59701fd0d4c
griptapeOsipa <osipa@griptape.ai>
2025-04-20 13:45:19 -0700

    node-name-updates-fixChecks

--- nodes/tests/unit/drivers/test_griptape_cloud_image_generation_driver.py
+++ nodes/tests/unit/drivers/test_griptape_cloud_image_generation_driver.py
@@ -80,14 +80,14 @@ class TestGriptapeCloudImageGenerationNode:
         ],
     )
     def test_adjust_size_based_on_model(self, model, size, expected_size) -> None:
-        griptape_cloud_image_generation_node = GriptapeCloudImageDriver(name="Griptape Cloud Image Generation")
+        griptape_cloud_image_generation_node = GriptapeCloudImage(name="Griptape Cloud Image Generation")
 
         adjusted_size = griptape_cloud_image_generation_node.adjust_size_based_on_model(model, size)
 
         assert adjusted_size == expected_size
 
     def test_process(self) -> None:
-        griptape_cloud_image_generation_node = GriptapeCloudImageDriver(name="Griptape Cloud Image Generation")
+        griptape_cloud_image_generation_node = GriptapeCloudImage(name="Griptape Cloud Image Generation")
 
         griptape_cloud_image_generation_node.process()
 
