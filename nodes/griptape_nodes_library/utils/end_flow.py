2789c7b43f8eac8ce737e21bfa116d9a0f174974
Jason Schleifer <jason.schleifer@gmail.com>
2025-04-23 08:43:28 +1200

    adding temporary input and output parameters (#573)

--- nodes/griptape_nodes_library/utils/end_flow.py
+++ nodes/griptape_nodes_library/utils/end_flow.py
@@ -1,6 +1,10 @@
 from typing import Any
 
-from griptape_nodes.exe_types.core_types import ControlParameterInput
+from griptape_nodes.exe_types.core_types import (
+    ControlParameterInput,
+    ParameterList,
+    ParameterMode,
+)
 from griptape_nodes.exe_types.node_types import BaseNode
 
 
@@ -12,6 +16,51 @@ class EndFlow(BaseNode):
     ) -> None:
         super().__init__(name, metadata)
         self.add_parameter(ControlParameterInput())
+        self.add_parameter(
+            ParameterList(
+                name="image",
+                input_types=["image"],
+                default_value=None,
+                tooltip="",
+                allowed_modes={ParameterMode.PROPERTY, ParameterMode.INPUT},
+            )
+        )
+        self.add_parameter(
+            ParameterList(
+                name="text",
+                input_types=["str"],
+                default_value=None,
+                tooltip="",
+                allowed_modes={ParameterMode.PROPERTY, ParameterMode.INPUT},
+            )
+        )
+        self.add_parameter(
+            ParameterList(
+                name="float",
+                input_types=["float"],
+                default_value=None,
+                tooltip="",
+                allowed_modes={ParameterMode.PROPERTY, ParameterMode.INPUT},
+            )
+        )
+        self.add_parameter(
+            ParameterList(
+                name="integer",
+                input_types=["int"],
+                default_value=None,
+                tooltip="",
+                allowed_modes={ParameterMode.PROPERTY, ParameterMode.INPUT},
+            )
+        )
+        self.add_parameter(
+            ParameterList(
+                name="boolean",
+                input_types=["bool"],
+                default_value=None,
+                tooltip="",
+                allowed_modes={ParameterMode.PROPERTY, ParameterMode.INPUT},
+            )
+        )
 
     def process(self) -> None:
         pass
