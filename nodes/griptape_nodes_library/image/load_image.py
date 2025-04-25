907e78d88dafb2e2bb18efdb0f4e0bea85f10ad9
James Clarendon <james.clarendon@gmail.com>
2025-04-24 21:56:05 -0700

    total hack workaround, need guidance for how to properly handle

--- nodes/griptape_nodes_library/image/load_image.py
+++ nodes/griptape_nodes_library/image/load_image.py
@@ -1,3 +1,5 @@
+from griptape.artifacts import ImageArtifact
+
 from griptape_nodes.exe_types.core_types import Parameter
 from griptape_nodes.exe_types.node_types import DataNode
 from griptape_nodes_library.utils.image_utils import dict_to_image_artifact
@@ -23,7 +25,15 @@ class LoadImage(DataNode):
 
     def process(self) -> None:
         image = self.parameter_values["image"]
-        # Convert to ImageArtifact
-        image_artifact = dict_to_image_artifact(image)
+        # Convert to ImageArtifact, if it's not one already.
+        if isinstance(image, ImageArtifact):
+            image_artifact = image  # No transform necessary.
+        elif isinstance(image, dict):
+            image_artifact = dict_to_image_artifact(
+                image
+            )  # would this ever happen? Do BlobArtifacts come in as dicts? Should we handle BlobArtifact separately?
+        else:
+            image_artifact = None  # I don't know how to handle this.
 
-        self.parameter_output_values["image"] = image_artifact
+        if image_artifact:
+            self.parameter_output_values["image"] = image_artifact
