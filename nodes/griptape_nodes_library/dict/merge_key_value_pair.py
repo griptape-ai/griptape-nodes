0d5b8b9120a96bce4d690ea3f489d289adf18e40
griptapeOsipa <osipa@griptape.ai>
2025-04-25 13:52:04 -0700

    lib cleaned and merge kvpair updated

--- nodes/griptape_nodes_library/dict/merge_key_value_pair.py
+++ nodes/griptape_nodes_library/dict/merge_key_value_pair.py
@@ -2,6 +2,7 @@ from typing import Any
 
 from griptape_nodes.exe_types.core_types import (
     Parameter,
+    ParameterList,
     ParameterMode,
 )
 from griptape_nodes.exe_types.node_types import DataNode
@@ -15,37 +16,13 @@ class MergeKeyValuePairs(DataNode):
     ) -> None:
         super().__init__(name, metadata)
 
-        # Add a list of inputs
         self.add_parameter(
-            Parameter(
-                name="key_value_pair_1",
-                allowed_modes={ParameterMode.INPUT, ParameterMode.PROPERTY},
+            ParameterList(
+                name="KeyValuePairs",
                 input_types=["dict"],
-                tooltip="Key/Value pair inputs to merge together.",
-            )
-        )
-        self.add_parameter(
-            Parameter(
-                name="key_value_pair_2",
-                allowed_modes={ParameterMode.INPUT, ParameterMode.PROPERTY},
-                input_types=["dict"],
-                tooltip="Key/Value pair inputs to merge together.",
-            )
-        )
-        self.add_parameter(
-            Parameter(
-                name="key_value_pair_3",
-                allowed_modes={ParameterMode.INPUT, ParameterMode.PROPERTY},
-                input_types=["dict"],
-                tooltip="Key/Value pair inputs to merge together.",
-            )
-        )
-        self.add_parameter(
-            Parameter(
-                name="key_value_pair_4",
-                allowed_modes={ParameterMode.INPUT, ParameterMode.PROPERTY},
-                input_types=["dict"],
-                tooltip="Key/Value pair inputs to merge together.",
+                default_value=None,
+                tooltip="Key Value Pair",
+                allowed_modes={ParameterMode.INPUT},
             )
         )
 
@@ -60,17 +37,20 @@ class MergeKeyValuePairs(DataNode):
             )
         )
 
-    def process(self) -> None:
-        # Get the list of input kvps
-        input_1 = self.parameter_values.get("key_value_pair_1", None)
-        input_2 = self.parameter_values.get("key_value_pair_2", None)
-        input_3 = self.parameter_values.get("key_value_pair_3", None)
-        input_4 = self.parameter_values.get("key_value_pair_4", None)
+    def get_kv_pairs(self) -> list:
+        kv_pairs = self.get_parameter_value("KeyValuePairs")
+        if kv_pairs:
+            if not isinstance(kv_pairs, list):
+                kv_pairs = [kv_pairs]
+            return kv_pairs
+        return []
 
+    def process(self) -> None:
         # Create a list of input texts if they aren't none
-        input_dicts = [input_1, input_2, input_3, input_4]
+        input_dicts = self.get_kv_pairs()
+
         # Filter out None values from the list
-        input_dicts = [text for text in input_dicts if text is not None]
+        # input_dicts = [text for text in input_dicts if text is not None]
 
         # Join all the kvps in to a single dict
         merged_dict = {}
