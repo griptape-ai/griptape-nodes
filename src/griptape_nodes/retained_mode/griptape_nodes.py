787921aad93c8ed36b0263b34b8521c8c875a47a
Collin Dutter <collin@griptape.ai>
2025-04-25 17:03:37 -0700

    fix: namespace venv under python version, clean up library name

--- src/griptape_nodes/retained_mode/griptape_nodes.py
+++ src/griptape_nodes/retained_mode/griptape_nodes.py
@@ -4,6 +4,7 @@ import importlib.util
 import io
 import json
 import logging
+import platform
 import re
 import subprocess
 import sys
@@ -4432,15 +4433,22 @@ class LibraryManager:
             if dependencies:
                 pip_install_flags = library_metadata.get("pip_install_flags", [])
                 # Create a virtual environment for the library
-                library_venv_path = xdg_data_home() / "griptape_nodes" / "venvs" / library_name
+                python_version = platform.python_version()
+                library_venv_path = (
+                    xdg_data_home()
+                    / "griptape_nodes"
+                    / "venvs"
+                    / python_version
+                    / library_name.replace(" ", "_").strip()
+                )
                 if library_venv_path.exists():
                     logger.debug("Virtual environment already exists at %s", library_venv_path)
                 else:
-                    subprocess.run(
-                        [sys.executable, "-m", "uv", "venv", library_venv_path, "--python", "3.12"],
+                    subprocess.run(  # noqa: S603
+                        [sys.executable, "-m", "uv", "venv", library_venv_path, "--python", python_version],
                         check=True,
                         text=True,
-                    )  # noqa: S603
+                    )
                     logger.debug("Created virtual environment at %s", library_venv_path)
 
                 # Grab the python executable from the virtual environment so that we can pip install there
