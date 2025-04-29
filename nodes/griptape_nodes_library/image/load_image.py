ed9b3f75fd5bbde034ee29285235faa99246c3eb
Jason Schleifer <jason.schleifer@gmail.com>
2025-04-30 07:11:09 +1200

    adding filepath to load image node

--- nodes/griptape_nodes_library/image/load_image.py
+++ nodes/griptape_nodes_library/image/load_image.py
@@ -1,7 +1,7 @@
 from griptape.artifacts import ImageUrlArtifact
 from griptape.loaders import ImageLoader
 
-from griptape_nodes.exe_types.core_types import Parameter
+from griptape_nodes.exe_types.core_types import Parameter, ParameterMode
 from griptape_nodes.exe_types.node_types import DataNode
 from griptape_nodes_library.utils.image_utils import dict_to_image_artifact
 
@@ -13,15 +13,32 @@ class LoadImage(DataNode):
         # Need to define the category
         self.category = "Image"
         self.description = "Load an image"
-        image_parameter = Parameter(
-            name="image",
-            input_types=["ImageArtifact", "BlobArtifact", "ImageUrlArtifact"],
-            type="ImageArtifact",
-            output_type="ImageArtifact",
-            ui_options={"clickable_file_browser": True, "expander": True},
-            tooltip="The image that has been generated.",
+
+        self.add_parameter(
+            Parameter(
+                name="filepath",
+                input_types=["str"],
+                type="str",
+                output_type="str",
+                ui_options={
+                    "clickable_file_browser": True,
+                    "filetypes": ["jpg", "jpeg", "png", "gif", "bmp", "webp"],
+                    "placeholder_text": "The path to the image file.",
+                },
+                tooltip="Path to the image file.",
+            )
         )
-        self.add_parameter(image_parameter)
+        self.add_parameter(
+            Parameter(
+                name="image",
+                input_types=["ImageArtifact", "BlobArtifact", "ImageUrlArtifact"],
+                type="ImageUrlArtifact",
+                output_type="ImageUrlArtifact",
+                ui_options={"clickable_file_browser": True, "expander": True},
+                allowed_modes={ParameterMode.OUTPUT, ParameterMode.PROPERTY},
+                tooltip="The image that has been generated.",
+            )
+        )
         # Add input parameter for model selection
 
     def process(self) -> None:
