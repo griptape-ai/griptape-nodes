d11542d1f537836e33ce1632648db1bf8d479e99
dylanholmes <4370153+dylanholmes@users.noreply.github.com>
2025-05-01 15:07:31 -0700

    Add pillow, diffusers, and spandrel default node libraries (#761)
    * Add pillow, diffusers, and spandrel default node libraries
    
    format
    
    nodes -> libraries in makefile + format
    
    fix lint
    
    Fix types and spell checks
    
    remove type temporarily
    
    going back
    
    * fix type check
    
    * fix checks
    
    * module doc comments
    
    * format

---
+++ nodes/pillow_nodes_library/utils.py
@@ -0,0 +1,24 @@
+import io
+
+import PIL.Image
+from griptape.artifacts import ImageArtifact
+from PIL.Image import Image
+
+
+def image_artifact_to_pil(image_artifact: ImageArtifact) -> Image:
+    """Converts Griptape ImageArtifact to Pillow Image."""
+    return PIL.Image.open(io.BytesIO(image_artifact.value))
+
+
+def pil_to_image_artifact(pil_image: Image) -> ImageArtifact:
+    """Converts Pillow Image to Griptape ImageArtifact."""
+    image_io = io.BytesIO()
+    pil_image.save(image_io, "PNG")
+    image_bytes = image_io.getvalue()
+    width, height = pil_image.size
+    return ImageArtifact(
+        value=image_bytes,
+        format="image/png",
+        width=width,
+        height=height,
+    )
