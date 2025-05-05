f153cad657a6eaf2a4e7f2b14eaff7e2ce3e24bb
Collin Dutter <collin@griptape.ai>
2025-05-05 16:56:49 -0700

    fix: add local bin uv to path

--- install.ps1
+++ install.ps1
@@ -22,6 +22,7 @@ ColorWrite "uv installed successfully." 'Green'
 
 ColorWrite "`nInstalling Griptape Nodes Engine...`n" 'Cyan'
 $uvPath = "$HOME\.local\bin\uv.exe"
+$env:Path = "$HOME\.local\bin;$env:Path"
 & $uvPath tool update-shell
 & $uvPath tool install --force --python python3.12 griptape-nodes > $null
 
