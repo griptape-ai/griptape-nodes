b2a91fcd8e776b611df3dca115a255d47558e184
Kate Forsberg <kate@griptape.ai>
2025-05-08 13:50:57 -0700

    one line

--- src/griptape_nodes/retained_mode/managers/event_manager.py
+++ src/griptape_nodes/retained_mode/managers/event_manager.py
@@ -82,7 +82,7 @@ class EventManager:
                     result_payload.altered_workflow_state = False
 
                 retained_mode_str = None
-                if depth_manager.is_top_level():
+                if depth_manager.is_top_level() and request.request_id is not None:
                     retained_mode_str = depth_manager.request_retained_mode_translation(request)
 
                 # Some requests have fields marked as "omit_from_result" which should be removed from the request
