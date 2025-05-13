7297fa54b8b74ac7950ec2213a506806345a96ad
Kate Forsberg <kate@griptape.ai>
2025-05-13 11:54:06 -0700

    trying new strategy

--- kate_scripts/for_loop.py
+++ kate_scripts/for_loop.py
@@ -1,5 +1,40 @@
+from griptape_nodes.retained_mode.events.connection_events import CreateConnectionRequest
+from griptape_nodes.retained_mode.events.context_events import SetWorkflowContextRequest
+from griptape_nodes.retained_mode.events.execution_events import StartFlowRequest
 from griptape_nodes.retained_mode.events.flow_events import CreateFlowRequest
+from griptape_nodes.retained_mode.events.library_events import RegisterLibraryFromFileRequest
+from griptape_nodes.retained_mode.events.node_events import CreateNodeRequest
+from griptape_nodes.retained_mode.events.parameter_events import SetParameterValueRequest
 from griptape_nodes.retained_mode.griptape_nodes import GriptapeNodes
 
-GriptapeNodes.handle_request(CreateFlowRequest(flow_name="Canvas",set_as_new_context=True, parent_flow_name=None))
-GriptapeNodes.handle_request(CreateFlowRequest(flow_name="SubFlow",set_as_new_context=True, parent_flow_name="Canvas"))
+GriptapeNodes.handle_request(RegisterLibraryFromFileRequest(file_path="/Users/kateforsberg/Griptape/griptape-nodes/libraries/griptape_nodes_library/griptape_nodes_library.json"))
+
+GriptapeNodes.ContextManager().push_workflow("My very unique workflow name.")
+
+GriptapeNodes.handle_request(CreateFlowRequest(flow_name="Canvas", parent_flow_name=None, set_as_new_context=True))
+GriptapeNodes.handle_request(CreateFlowRequest(flow_name="SubFlow_1", set_as_new_context=True, parent_flow_name="Canvas"))
+
+# Create an Agent node in the subflow
+GriptapeNodes.handle_request(CreateNodeRequest(node_type="Agent", node_name="GPT_Agent", specific_library_name="Griptape Nodes Library"))
+
+# Create an image generation node in the subflow
+GriptapeNodes.handle_request(CreateNodeRequest(node_type="GenerateImage", node_name="Image_Generator", specific_library_name="Griptape Nodes Library"))
+
+# Connect the Agent to the Image Generator
+GriptapeNodes.handle_request(CreateConnectionRequest(source_node_name="GPT_Agent", source_parameter_name="exec_out", target_node_name="Image_Generator", target_parameter_name="exec_in"))
+GriptapeNodes.handle_request(CreateConnectionRequest(source_node_name="GPT_Agent", source_parameter_name="output", target_node_name="Image_Generator", target_parameter_name="prompt"))
+
+#Go back to Canvas
+GriptapeNodes.ContextManager().pop_flow()
+
+# Create a ForLoop node in the Canvas flow
+GriptapeNodes.handle_request(CreateNodeRequest(node_type="ForLoop", node_name="Loop_Controller", specific_library_name="Griptape Nodes Library"))
+
+# Set the flow parameter of the ForLoop to point to SubFlow_1
+subflow_1 = GriptapeNodes.FlowManager().get_flow_by_name("SubFlow_1")
+GriptapeNodes.handle_request(SetParameterValueRequest(node_name="Loop_Controller", parameter_name="flow", value=subflow_1))
+
+# Set some test prompts for the loop
+GriptapeNodes.handle_request(SetParameterValueRequest(node_name="Loop_Controller", parameter_name="looped_input", value=["A beautiful sunset", "A majestic mountain", "A peaceful lake"], data_type="list"))
+
+GriptapeNodes.handle_request(StartFlowRequest(flow_name="Canvas"))
--- libraries/griptape_nodes_library/griptape_nodes_library/execution/for_loop.py
+++ libraries/griptape_nodes_library/griptape_nodes_library/execution/for_loop.py
@@ -1,36 +1,41 @@
+import time
 from typing import Any, cast
 
 from griptape_nodes.exe_types.core_types import (
+    ControlParameter,
+    ControlParameterOutput,
     Parameter,
     ParameterList,
     ParameterMode,
 )
 from griptape_nodes.exe_types.flow import ControlFlow
-from griptape_nodes.exe_types.node_types import ControlNode
+from griptape_nodes.exe_types.node_types import BaseNode, ControlNode, NodeResolutionState
 from griptape_nodes.retained_mode.events.execution_events import StartFlowRequest
 from griptape_nodes.retained_mode.griptape_nodes import GriptapeNodes
 
 
-class ForLoop(ControlNode):
+class RepeatNode(BaseNode):
+    times: int
+    sub_flow: ControlFlow | None
     def __init__(
         self,
         name: str,
         metadata: dict[Any, Any] | None = None,
     ) -> None:
         super().__init__(name, metadata)
+        self.sub_flow = None
         self.add_parameter(
-            Parameter(
-                name="looped_input",
-                input_types=["list[str]"],
-                type="str",
-                default_value=[],
+            ParameterList(
+                name="for_each",
+                input_types=["str"],
+                default_value=None,
                 tooltip="",
                 allowed_modes={ParameterMode.PROPERTY, ParameterMode.INPUT},
             )
         )
         self.add_parameter(
             ParameterList(
-                name="list_output",
+                name="output",
                 tooltip = "",
                 type="ImageArtifact",
                 output_type="ImageArtifact",
@@ -38,29 +43,75 @@ class ForLoop(ControlNode):
                 settable=True
             )
         )
-        self.add_parameter(
-            Parameter(
-                name="flow",
-                tooltip="The flow to rerun",
-                type="ControlFlow",
-                allowed_modes={ParameterMode.INPUT}
+        self.repeat_parameter = ControlParameterOutput(
+                name="repeat",
+                tooltip="Continue iteration",
             )
+        self.repeat_parameter.ui_options = {"display_name":"Repeat"}
+        self.add_parameter(
+            self.repeat_parameter
         )
+        self.special_subflow = ControlParameter(
+            name="SubFlow_Connection",
+            tooltip="Connect to a subflow",
+        )
+        self.add_parameter(
+            self.special_subflow
+        )
+        self.continue_parameter = ControlParameterOutput(
+            name="Continue_On",
+            tooltip="Continue down flow"
+        )
+        self.add_parameter(self.continue_parameter)
+        self.times = 0
 
     def process(self) -> None:
+        flow_name = GriptapeNodes.NodeManager().get_node_parent_flow_by_name(self.name)
+        flow = GriptapeNodes.FlowManager().get_flow_by_name(flow_name)
+        exec_in = self.get_parameter_by_name("exec_in")
+        if exec_in is not None and flow is not None:
+            flow.add_connection(self,self.iterate_more,self,exec_in)
         loop = self.get_parameter_value("looped_input")
+        self.loop = len(loop)
         # Get the list we are going to loop over. ForEach Loop
         flow = self.get_parameter_value("flow")
-        for prompt in loop:
+        if flow is not None:
             flow = cast("ControlFlow", flow)
-            start_node_queue = flow.get_start_node_queue()
-            start_node = start_node_queue.get() if start_node_queue is not None else None
-            if start_node is not None:
-                start_node = cast("ControlNode", start_node)
-                start_node.set_parameter_value("prompt",prompt)
-                GriptapeNodes.handle_request(StartFlowRequest("SubFlow_1"))
-            # Flow then executes. Hooray!
-            end_node = flow.end_node
-            if end_node is not None:
-                self.parameter_output_values["list_output"] = end_node.parameter_output_values["output"]
+            self.parameter_output_values["list_output"] = []
+            for prompt in loop:
+                start_node_queue = flow.get_start_node_queue()
+                start_node = start_node_queue.queue[0] if start_node_queue is not None and not start_node_queue.empty() else None
+                if start_node is not None:
+                    start_node = cast("ControlNode", start_node)
+                    start_node.set_parameter_value("prompt",prompt)
+                    #yield lambda: self._process(flow=flow)
+                    flow.start_flow(start_node)
+                    while flow.check_for_existing_running_flow():
+                        time.sleep(0.1)
+                    self.loop = self.loop - 1
+                    end_node = flow.end_node
+                    if end_node is not None:
+                        self.parameter_output_values["list_output"].append(end_node.parameter_output_values["output"])
+
+
+    def get_next_control_output(self) -> Parameter | None:
+        while self.times > 0:
+            # Set the state to unresolved so it will run again.
+            self.state = NodeResolutionState.UNRESOLVED
+            if self.sub_flow:
+                self.sub_flow.unresolve_whole_flow()
+            return self.repeat_parameter
+        # Continue on in the main flow
+        return self.get_parameter_by_name("exec_out")
+
+
+    # def _process(self, flow:ControlFlow) -> ControlFlow:
+    #     GriptapeNodes.handle_request(StartFlowRequest("SubFlow_1"))
+    #         # Flow then executes. Hooray!
+    #     while flow.check_for_existing_running_flow():
+    #         time.sleep(0.1)
+    #     end_node = flow.end_node
+    #     if end_node is not None:
+    #         self.parameter_output_values["list_output"].append(end_node.parameter_output_values["output"])
+    #     return flow
 
