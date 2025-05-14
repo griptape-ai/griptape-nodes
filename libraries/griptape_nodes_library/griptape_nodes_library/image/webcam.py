91923b07b981f4b0cc367acc5832ef3d551b5f31
Andrew French <andrew@afren.ch>
2025-05-13 17:38:06 -0700

    Add webcam node

---
+++ libraries/griptape_nodes_library/griptape_nodes_library/image/webcam.py
@@ -0,0 +1,32 @@
+from griptape_nodes.exe_types.core_types import Parameter, ParameterMode
+from griptape_nodes.exe_types.node_types import DataNode
+from griptape_nodes_library.utils.image_utils import dict_to_image_url_artifact
+
+
+class Webcam(DataNode):
+    def __init__(self, **kwargs) -> None:
+        super().__init__(**kwargs)
+
+        # Need to define the category
+        self.category = "Image"
+        self.description = "Capture an image"
+        image_parameter = Parameter(
+            name="image",
+            input_types=["ImageArtifact", "ImageUrlArtifact"],
+            type="ImageArtifact",
+            output_type="ImageUrlArtifact",
+            ui_options={"webcam_capture_image": True, "expander": True},
+            tooltip="The image that has been captured.",
+            allowed_modes={ParameterMode.OUTPUT}
+        )
+        self.add_parameter(image_parameter)
+
+    def process(self) -> None:
+        image = self.parameter_values["image"]
+
+        if isinstance(image, dict):
+            image_artifact = dict_to_image_url_artifact(image)
+        else:
+            image_artifact = image
+
+        self.parameter_output_values["image"] = image_artifact
