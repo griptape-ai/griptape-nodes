e6e0d0d5211a2d1fb794f7bc81f03a9a6647162c
dylanholmes <4370153+dylanholmes@users.noreply.github.com>
2025-05-14 06:18:47 -0700

    remove help parameter

--- libraries/griptape_nodes_advanced_media_library/diffusers_nodes_library/common/parameters/huggingface_model_parameter.py
+++ libraries/griptape_nodes_advanced_media_library/diffusers_nodes_library/common/parameters/huggingface_model_parameter.py
@@ -2,7 +2,6 @@ import logging
 from abc import ABC, abstractmethod
 
 from diffusers_nodes_library.common.utils.option_utils import update_option_choices
-from diffusers_nodes_library.common.utils.ui_option_utils import update_ui_option_hide
 from griptape_nodes.exe_types.core_types import Parameter, ParameterMode
 from griptape_nodes.exe_types.node_types import BaseNode
 from griptape_nodes.traits.options import Options
@@ -25,7 +24,6 @@ class HuggingFaceModelParameter(ABC):
     def __init__(self, node: BaseNode, parameter_name: str):
         self._node = node
         self._parameter_name = parameter_name
-        self._message_parameter_name = f"{parameter_name}_help_message"
         self._repo_revisions = []
 
     def refresh_parameters(self) -> None:
@@ -33,34 +31,14 @@ class HuggingFaceModelParameter(ABC):
         self._repo_revisions = self.fetch_repo_revisions()
         num_repo_revisions_after = len(self.list_repo_revisions())
 
-        if num_repo_revisions_before != num_repo_revisions_after:
-            if self._node.get_parameter_by_name(self._message_parameter_name):
-                # We have new repo revisions.
-                # 1. Hide the message parameter if it exists
-                update_ui_option_hide(self._node, self._message_parameter_name, hide=bool(self._repo_revisions))
-            if self._node.get_parameter_by_name(self._parameter_name):
-                # 2. Update the repo revision selection parameter with the new choices
-                choices = self.get_choices()
-                update_option_choices(self._node, self._parameter_name, choices, choices[0])
+        if num_repo_revisions_before != num_repo_revisions_after and self._node.get_parameter_by_name(
+            self._parameter_name
+        ):
+            choices = self.get_choices()
+            update_option_choices(self._node, self._parameter_name, choices, choices[0])
 
     def add_input_parameters(self) -> None:
         self._repo_revisions = self.fetch_repo_revisions()
-        self._node.add_parameter(
-            Parameter(
-                name=self._message_parameter_name,
-                type="str",
-                default_value=self.get_help_message(),
-                tooltip="",
-                allowed_modes={},  # type: ignore  # noqa: PGH003
-                ui_options={
-                    "is_full_width": True,
-                    "multiline": True,
-                    "is_read_only": True,
-                    "hide": bool(self._repo_revisions),
-                },
-            )
-        )
-
         choices = self.get_choices()
         self._node.add_parameter(
             Parameter(
@@ -96,7 +74,7 @@ class HuggingFaceModelParameter(ABC):
     def get_repo_revision(self) -> tuple[str, str]:
         value = self._node.get_parameter_value(self._parameter_name)
         if value is None:
-            logger.exception("No %s specified", self._parameter_name)
+            raise RuntimeError(self.get_help_message())
         base_repo_id, base_revision = self._key_to_repo_revision(value)
         return base_repo_id, base_revision
 
