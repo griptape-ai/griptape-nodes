573a08b21b5843de4f6705ebfb3992319683ec0a
James Clarendon <SavagePencil@users.noreply.github.com>
2025-05-20 08:25:05 -0700

    Adds a Sandbox Library setting that automatically loads Python files located within a Sandbox directory (#1250)
    * stopping point
    
    * lousy Path objects
    
    * Merge and address comments.

--- src/griptape_nodes/retained_mode/managers/settings.py
+++ src/griptape_nodes/retained_mode/managers/settings.py
@@ -54,6 +54,10 @@ class Settings(BaseModel):
         default="staticfiles",
         description="Path to the static files directory, relative to the workspace directory.",
     )
+    sandbox_library_directory: str = Field(
+        default="sandbox_library",
+        description="Path to the sandbox library directory (useful while developing nodes). If presented as just a directory (e.g., 'sandbox_library') it will be interpreted as being relative to the workspace directory.",
+    )
     app_events: AppEvents = Field(default_factory=AppEvents)
     nodes: dict[str, Any] = Field(
         default_factory=lambda: {
