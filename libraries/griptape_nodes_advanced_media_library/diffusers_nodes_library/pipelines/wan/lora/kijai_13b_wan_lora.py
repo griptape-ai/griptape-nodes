073f30128bb2d953a36068edbb6a3e45b44bfd66
dylanholmes <4370153+dylanholmes@users.noreply.github.com>
2025-06-03 21:53:32 +0000

    wan wip

---
+++ libraries/griptape_nodes_advanced_media_library/diffusers_nodes_library/pipelines/wan/lora/kijai_13b_wan_lora.py
@@ -0,0 +1,22 @@
+import logging
+from typing import override
+
+from diffusers_nodes_library.pipelines.wan.lora.huggingface_wan_lora import (
+    HuggingFaceWanLora,  # type: ignore[reportMissingImports]
+)
+
+logger = logging.getLogger("diffusers_nodes_library")
+
+
+class Kijai13bWanLora(HuggingFaceWanLora):
+    @override
+    def get_repo_id(self) -> str:
+        return "Kijai/WanVideo_comfy"
+
+    @override
+    def get_filename(self) -> str:
+        return "Wan21_CausVid_bidirect2_T2V_1_3B_lora_rank32.safetensors"
+
+    @override
+    def get_trigger_phrase(self) -> str | None:
+        return None
---
+++ libraries/griptape_nodes_advanced_media_library/diffusers_nodes_library/pipelines/wan/wan_pipeline_parameters.py
@@ -0,0 +1,211 @@
+import logging
+from pathlib import Path
+from typing import Any
+import uuid
+
+from griptape_nodes.retained_mode.griptape_nodes import GriptapeNodes
+
+import diffusers  # type: ignore[reportMissingImports]
+import PIL.Image
+from PIL.Image import Image
+
+from griptape_nodes_library.video.video_url_artifact import VideoUrlArtifact  # type: ignore[reportMissingImports]
+from pillow_nodes_library.utils import pil_to_image_artifact  # type: ignore[reportMissingImports]
+
+from diffusers_nodes_library.common.parameters.huggingface_repo_parameter import HuggingFaceRepoParameter
+from diffusers_nodes_library.common.parameters.seed_parameter import SeedParameter
+from griptape_nodes.exe_types.core_types import Parameter, ParameterMode
+from griptape_nodes.exe_types.node_types import BaseNode
+
+logger = logging.getLogger("diffusers_nodes_library")
+
+
+class WanPipelineParameters:
+    def __init__(self, node: BaseNode):
+        self._node = node
+        self._huggingface_repo_parameter = HuggingFaceRepoParameter(
+            node,
+            repo_ids=[
+                "Wan-AI/Wan2.1-T2V-1.3B-Diffusers",
+                "Wan-AI/Wan2.1-T2V-14B-Diffusers",
+            ],
+        )
+        self._seed_parameter = SeedParameter(node)
+
+    def add_input_parameters(self) -> None:
+        self._huggingface_repo_parameter.add_input_parameters()
+        self._node.add_parameter(
+            Parameter(
+                name="text_encoder",
+                input_types=["str"],
+                type="str",
+                allowed_modes=set(),
+                tooltip="text_encoder",
+                default_value="todo",
+            )
+        )
+        self._node.add_parameter(
+            Parameter(
+                name="prompt",
+                default_value="",
+                input_types=["str"],
+                type="str",
+                tooltip="prompt",
+            )
+        )
+        self._node.add_parameter(
+            Parameter(
+                name="negative_prompt",
+                default_value="",
+                input_types=["str"],
+                type="str",
+                tooltip="optional negative_prompt",
+            )
+        )
+        self._node.add_parameter(
+            Parameter(
+                name="true_cfg_scale",
+                default_value=1.0,
+                input_types=["float"],
+                type="float",
+                tooltip="true_cfg_scale",
+            )
+        )
+        self._node.add_parameter(
+            Parameter(
+                name="width",
+                default_value=512,
+                input_types=["int"],
+                type="int",
+                tooltip="width",
+            )
+        )
+        self._node.add_parameter(
+            Parameter(
+                name="height",
+                default_value=480,
+                input_types=["int"],
+                type="int",
+                tooltip="height",
+            )
+        )
+        self._node.add_parameter(
+            Parameter(
+                name="num_frames",
+                default_value=33,
+                input_types=["int"],
+                type="int",
+                tooltip="num_frames",
+            )
+        )
+        self._node.add_parameter(
+            Parameter(
+                name="num_inference_steps",
+                default_value=50,
+                input_types=["int"],
+                type="int",
+                tooltip="num_inference_steps",
+            )
+        )
+        # TODO: https://github.com/griptape-ai/griptape-nodes/issues/841
+        self._node.add_parameter(
+            Parameter(
+                name="guidance_scale",
+                default_value=5.0,
+                input_types=["float"],
+                type="float",
+                tooltip="guidance_scale",
+            )
+        )
+        self._seed_parameter.add_input_parameters()
+
+    def add_output_parameters(self) -> None:
+        self._node.add_parameter(
+            Parameter(
+                name="output_video",
+                output_type="VideoUrlArtifact",
+                tooltip="The output video",
+                allowed_modes={ParameterMode.OUTPUT},
+            )
+        )
+
+    def validate_before_node_run(self) -> list[Exception] | None:
+        errors = self._huggingface_repo_parameter.validate_before_node_run()
+        return errors or None
+
+    def after_value_set(self, parameter: Parameter, value: Any, modified_parameters_set: set[str]) -> None:
+        self._seed_parameter.after_value_set(parameter, value, modified_parameters_set)
+
+    def preprocess(self) -> None:
+        self._seed_parameter.preprocess()
+
+    def get_repo_revision(self) -> tuple[str, str]:
+        return self._huggingface_repo_parameter.get_repo_revision()
+
+    def publish_output_image_preview_placeholder(self) -> None:
+        width = int(self._node.parameter_values["width"])
+        height = int(self._node.parameter_values["height"])
+        # Immediately set a preview placeholder image to make it react quickly and adjust
+        # the size of the image preview on the node.
+        preview_placeholder_image = PIL.Image.new("RGB", (width, height), color="black")
+        self._node.publish_update_to_parameter("output_image", pil_to_image_artifact(preview_placeholder_image))
+
+    def get_prompt(self) -> str:
+        return self._node.get_parameter_value("prompt")
+
+    def get_negative_prompt(self) -> str:
+        return self._node.get_parameter_value("negative_prompt")
+
+    def get_true_cfg_scale(self) -> float:
+        return float(self._node.get_parameter_value("true_cfg_scale"))
+
+    def get_width(self) -> int:
+        return int(self._node.get_parameter_value("width"))
+
+    def get_height(self) -> int:
+        return int(self._node.get_parameter_value("height"))
+    
+    def get_num_frames(self) -> int:
+        return int(self._node.get_parameter_value("num_frames"))
+
+    def get_num_inference_steps(self) -> int:
+        return int(self._node.get_parameter_value("num_inference_steps"))
+
+    def get_guidance_scale(self) -> float:
+        return float(self._node.get_parameter_value("guidance_scale"))
+
+    def get_pipe_kwargs(self) -> dict:
+        return {
+            "prompt": self.get_prompt(),
+            "negative_prompt": self.get_negative_prompt(),
+            "width": self.get_width(),
+            "height": self.get_height(),
+            "num_frames": self.get_num_frames(),
+            "num_inference_steps": self.get_num_inference_steps(),
+            "guidance_scale": self.get_guidance_scale(),
+            "generator": self._seed_parameter.get_generator(),
+        }
+
+    def latents_to_image_pil(
+        self, pipe: diffusers.WanPipeline, latents: Any
+    ) -> Image:
+        width = int(self._node.parameter_values["width"])
+        height = int(self._node.parameter_values["height"])
+        latents = pipe._unpack_latents(latents, height, width, pipe.vae_scale_factor)
+        latents = (latents / pipe.vae.config.scaling_factor) + pipe.vae.config.shift_factor
+        image = pipe.vae.decode(latents, return_dict=False)[0]
+        # TODO: https://github.com/griptape-ai/griptape-nodes/issues/845
+        intermediate_pil_image = pipe.image_processor.postprocess(image, output_type="pil")[0]
+        return intermediate_pil_image
+
+    def publish_output_image_preview_latents(
+        self, pipe: diffusers.WanPipeline, latents: Any
+    ) -> None:
+        preview_image_pil = self.latents_to_image_pil(pipe, latents)
+        preview_image_artifact = pil_to_image_artifact(preview_image_pil)
+        self._node.publish_update_to_parameter("output_image", preview_image_artifact)
+
+    def publish_output_video(self, video_file: Path) -> None:
+        filename = f"{uuid.uuid4()}{video_file.suffix}"
+        url = GriptapeNodes.StaticFilesManager().save_static_file(video_file.read_bytes(), filename)
+        self._node.parameter_output_values["output_video"] = VideoUrlArtifact(url)
