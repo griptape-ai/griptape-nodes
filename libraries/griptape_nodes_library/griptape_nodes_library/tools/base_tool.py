cc5e85543a090af8c3526a48eed29ce6b3fc91b3
Jason Schleifer <jason.schleifer@gmail.com>
2025-06-06 06:51:23 +1200

    adding helpful info

--- libraries/griptape_nodes_library/griptape_nodes_library/tools/base_tool.py
+++ libraries/griptape_nodes_library/griptape_nodes_library/tools/base_tool.py
@@ -1,7 +1,11 @@
+from typing import Literal
+
 from griptape.tools import BaseTool as GtBaseTool
 
-from griptape_nodes.exe_types.core_types import Parameter, ParameterMode
+from griptape_nodes.exe_types.core_types import Parameter, ParameterMessage, ParameterMode
 from griptape_nodes.exe_types.node_types import DataNode
+
+VariantType = Literal["info", "warning", "error", "success", "tip", "none"]
 
 
 class BaseTool(DataNode):
@@ -16,6 +20,14 @@ class BaseTool(DataNode):
 
     def __init__(self, name: str, metadata: dict | None = None) -> None:
         super().__init__(name, metadata)
+        self.add_node_element(
+            ParameterMessage(
+                name="tool_info",
+                variant="info",
+                value="This tool can be provided to an agent to help it perform tasks.",
+                ui_options={"full_width": True},
+            )
+        )
 
         self.add_parameter(
             Parameter(
@@ -38,6 +50,16 @@ class BaseTool(DataNode):
                 tooltip="",
             )
         )
+
+    def update_tool_info(self, value: str = "", title: str = "", variant: VariantType = "tip") -> None:
+        message = self.get_message_by_name_or_element_id("tool_info")
+        if message:
+            if value:
+                message.value = value
+            if title:
+                message.title = title
+            if variant:
+                message.variant = variant
 
     def process(self) -> None:
         off_prompt = self.parameter_values.get("off_prompt", False)
