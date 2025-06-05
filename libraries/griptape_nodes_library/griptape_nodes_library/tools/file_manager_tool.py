38ea8144ed871b0de2ba602448091732b97b0a85
Jason Schleifer <jason.schleifer@gmail.com>
2025-06-06 08:39:21 +1200

    updating the tools

--- libraries/griptape_nodes_library/griptape_nodes_library/tools/file_manager_tool.py
+++ libraries/griptape_nodes_library/griptape_nodes_library/tools/file_manager_tool.py
@@ -1,9 +1,31 @@
 from griptape.tools import FileManagerTool as GtFileManagerTool
 
+from griptape_nodes.exe_types.core_types import Parameter
+from griptape_nodes.traits.options import Options
 from griptape_nodes_library.tools.base_tool import BaseTool
 
 
 class FileManager(BaseTool):
+    def __init__(self, **kwargs) -> None:
+        super().__init__(**kwargs)
+
+        self.update_tool_info(
+            value="The FileManager tool can be given to an agent to help it perform file operations.",
+            title="FileManager Tool",
+        )
+
+        self.add_parameter(
+            Parameter(
+                name="file_location",
+                type="str",
+                tooltip="The location of the files to be used by the tool.",
+                default_value="local",
+                traits={Options(choices=["local", "GriptapeCloud"])},
+            )
+        )
+        self.parameters.sort(key=lambda p: p.name)
+        self.hide_parameter_by_name("off_prompt")
+
     def process(self) -> None:
         off_prompt = self.parameter_values.get("off_prompt", True)
 
--- libraries/griptape_nodes_library/griptape_nodes_library/tools/web_scraper_tool.py
+++ libraries/griptape_nodes_library/griptape_nodes_library/tools/web_scraper_tool.py
@@ -1,66 +1,22 @@
-from griptape.artifacts import ListArtifact
-from griptape.tasks import PromptTask
 from griptape.tools import WebScraperTool as GtWebScraperTool
 
-from griptape_nodes.exe_types.core_types import Parameter, ParameterMode
-from griptape_nodes.exe_types.node_types import ControlNode
+from griptape_nodes_library.tools.base_tool import BaseTool
 
 
-class WebScraper(ControlNode):
+class WebScraper(BaseTool):
     def __init__(self, **kwargs) -> None:
         super().__init__(**kwargs)
-        self.add_parameter(
-            Parameter(
-                name="url",
-                input_types=["str"],
-                type="str",
-                output_type="str",
-                default_value="",
-                tooltip="",
-            )
+
+        self.update_tool_info(
+            value="The WebScraper tool can be given to an agent to help it perform web scraping operations.",
+            title="WebScraper Tool",
         )
-        self.add_parameter(
-            Parameter(
-                name="output",
-                input_types=["str"],
-                type="str",
-                output_type="str",
-                default_value="",
-                tooltip="",
-                ui_options={"multiline": True, "placeholder_text": "Output from the web scraper."},
-            )
-        )
-        self.add_parameter(
-            Parameter(
-                name="tool",
-                input_types=["Tool"],
-                type="Tool",
-                output_type="Tool",
-                default_value=None,
-                allowed_modes={ParameterMode.OUTPUT},
-                tooltip="",
-            )
-        )
+        self.hide_parameter_by_name("off_prompt")
 
     def process(self) -> None:
         off_prompt = self.get_parameter_value("off_prompt")
-        url = self.get_parameter_value("url")
-
         # Create the tool
         tool = GtWebScraperTool(off_prompt=off_prompt)
-        scrape_task = PromptTask(
-            tools=[tool],
-            reflect_on_tool_use=False,
-        )
-
-        # Run the task
-        output = ""
-        response = scrape_task.run(url)
-        if isinstance(response, ListArtifact):
-            output += str(response[0].value[0].value)
-
-        # Set the output
-        self.parameter_output_values["output"] = output
 
         # Set the output
         self.parameter_output_values["tool"] = tool
