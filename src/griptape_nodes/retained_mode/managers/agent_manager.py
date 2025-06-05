a642e09cb601c48367f4040bb5c58b586c6656a4
CJ Kindel <cjkindel@users.noreply.github.com>
2025-06-05 13:59:40 -0700

    Basic events for chat Agents (#1407)
    * Basic events for chat Agents
    
    * format
    
    * Add model and conversation memory config
    
    * format
    
    * format more
    
    * change to PromptTask
    
    * Handle no api key
    
    * handle ErrorArtifact
    
    * more specific config events
    
    * explicit ResetAgentConversationMemoryRequest event
    
    * change config agent event to generic

---
+++ src/griptape_nodes/retained_mode/managers/agent_manager.py
@@ -0,0 +1,93 @@
+import logging
+
+from griptape.artifacts import ErrorArtifact
+from griptape.drivers import GriptapeCloudPromptDriver
+from griptape.memory.structure import ConversationMemory
+from griptape.tasks import PromptTask
+
+from griptape_nodes.retained_mode.events.agent_events import (
+    ConfigureAgentRequest,
+    ConfigureAgentResultFailure,
+    ConfigureAgentResultSuccess,
+    GetConversationMemoryRequest,
+    GetConversationMemoryResultFailure,
+    GetConversationMemoryResultSuccess,
+    ResetAgentConversationMemoryRequest,
+    ResetAgentConversationMemoryResultFailure,
+    ResetAgentConversationMemoryResultSuccess,
+    RunAgentRequest,
+    RunAgentResultFailure,
+    RunAgentResultSuccess,
+)
+from griptape_nodes.retained_mode.events.base_events import ResultPayload
+from griptape_nodes.retained_mode.managers.config_manager import ConfigManager
+from griptape_nodes.retained_mode.managers.event_manager import EventManager
+from griptape_nodes.retained_mode.managers.secrets_manager import SecretsManager
+
+logger = logging.getLogger("griptape_nodes")
+
+API_KEY_ENV_VAR = "GT_CLOUD_API_KEY"
+SERVICE = "Griptape"
+
+config_manager = ConfigManager()
+secrets_manager = SecretsManager(config_manager)
+
+
+class AgentManager:
+    def __init__(self, event_manager: EventManager | None = None) -> None:
+        self.conversation_memory = ConversationMemory()
+        api_key = secrets_manager.get_secret(API_KEY_ENV_VAR)
+        if not api_key:
+            msg = f"Secret '{API_KEY_ENV_VAR}' not found"
+            raise ValueError(msg)
+        self.prompt_driver = GriptapeCloudPromptDriver(api_key=api_key)
+
+        if event_manager is not None:
+            event_manager.assign_manager_to_request_type(RunAgentRequest, self.on_handle_run_agent_request)
+            event_manager.assign_manager_to_request_type(ConfigureAgentRequest, self.on_handle_configure_agent_request)
+            event_manager.assign_manager_to_request_type(
+                ResetAgentConversationMemoryRequest, self.on_handle_reset_agent_conversation_memory_request
+            )
+            event_manager.assign_manager_to_request_type(
+                GetConversationMemoryRequest, self.on_handle_get_conversation_memory_request
+            )
+
+    def on_handle_run_agent_request(self, request: RunAgentRequest) -> ResultPayload:
+        task_output = PromptTask(
+            request.input, prompt_driver=self.prompt_driver, conversation_memory=self.conversation_memory
+        ).run()
+        if isinstance(task_output, ErrorArtifact):
+            details = f"Error running agent: {task_output.value}"
+            logger.error(details)
+            return RunAgentResultFailure(error=task_output.to_json())
+        return RunAgentResultSuccess(output=task_output.to_json())
+
+    def on_handle_configure_agent_request(self, request: ConfigureAgentRequest) -> ResultPayload:
+        try:
+            for key, value in request.prompt_driver.items():
+                setattr(self.prompt_driver, key, value)
+        except Exception as e:
+            details = f"Error configuring agent: {e}"
+            logger.error(details)
+            return ConfigureAgentResultFailure()
+        return ConfigureAgentResultSuccess()
+
+    def on_handle_reset_agent_conversation_memory_request(
+        self, _: ResetAgentConversationMemoryRequest
+    ) -> ResultPayload:
+        try:
+            self.conversation_memory = ConversationMemory()
+        except Exception as e:
+            details = f"Error resetting agent conversation memory: {e}"
+            logger.error(details)
+            return ResetAgentConversationMemoryResultFailure()
+        return ResetAgentConversationMemoryResultSuccess()
+
+    def on_handle_get_conversation_memory_request(self, _: GetConversationMemoryRequest) -> ResultPayload:
+        try:
+            conversation_memory = self.conversation_memory.runs
+        except Exception as e:
+            details = f"Error getting conversation memory: {e}"
+            logger.error(details)
+            return GetConversationMemoryResultFailure()
+        return GetConversationMemoryResultSuccess(runs=conversation_memory)
