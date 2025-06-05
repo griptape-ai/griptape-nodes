2b9fd3fe438122d5944890736b654b6478dd27e8
Jason Schleifer <jason.schleifer@gmail.com>
2025-06-06 11:48:05 +1200

    adding file manager location

--- libraries/griptape_nodes_library/griptape_nodes_library/tools/file_manager_tool.py
+++ libraries/griptape_nodes_library/griptape_nodes_library/tools/file_manager_tool.py
@@ -1,8 +1,12 @@
+from griptape.drivers.file_manager.local import LocalFileManagerDriver
 from griptape.tools import FileManagerTool as GtFileManagerTool
 
 from griptape_nodes.exe_types.core_types import Parameter
+from griptape_nodes.retained_mode.griptape_nodes import GriptapeNodes
 from griptape_nodes.traits.options import Options
 from griptape_nodes_library.tools.base_tool import BaseTool
+
+LOCATIONS = ["Local", "GriptapeCloud"]
 
 
 class FileManager(BaseTool):
@@ -19,18 +23,24 @@ class FileManager(BaseTool):
                 name="file_location",
                 type="str",
                 tooltip="The location of the files to be used by the tool.",
-                default_value="local",
-                traits={Options(choices=["local", "GriptapeCloud"])},
+                default_value=LOCATIONS[0],
+                traits={Options(choices=LOCATIONS)},
             )
         )
-        self.move_parameter_up_down("tool", up=False)
+        self.swap_parameters("file_location", "tool")
         self.hide_parameter_by_name("off_prompt")
 
     def process(self) -> None:
         off_prompt = self.parameter_values.get("off_prompt", True)
+        file_location = self.parameter_values.get("file_location")
 
+        # Get the setting for Workspace Directory
+        workdir = GriptapeNodes.ConfigManager().get_config_value("workspace_directory")
+
+        # gtc_driver = GriptapeCloudFileManagerDriver()
+        local_driver = LocalFileManagerDriver(workdir=workdir)
         # Create the tool
-        tool = GtFileManagerTool(off_prompt=off_prompt)
+        tool = GtFileManagerTool(file_manager_driver=local_driver, off_prompt=off_prompt)
 
         # Set the output
         self.parameter_output_values["tool"] = tool
