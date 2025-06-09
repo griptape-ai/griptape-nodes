fe3cef069467ec96dcec2635e369389b5c18286f
Collin Dutter <collin@griptape.ai>
2025-06-09 16:11:31 -0700

    fix: lazily instantiate loggers to avoid dupe handler registration (#1465)
    Quirk of supporting sse and websockets. Better fix will come when we get
    rid of the sse app.

--- src/griptape_nodes/app/app_websocket.py
+++ src/griptape_nodes/app/app_websocket.py
@@ -79,12 +79,6 @@ class EventLogHandler(logging.Handler):
         )
 
 
-griptape_nodes_logger = logging.getLogger("griptape_nodes")
-# When running as an app, we want to forward all log messages to the event queue so they can be sent to the GUI
-griptape_nodes_logger.addHandler(EventLogHandler())
-griptape_nodes_logger.addHandler(RichHandler(show_time=True, show_path=False, markup=True, rich_tracebacks=True))
-griptape_nodes_logger.setLevel(logging.INFO)
-
 # Logger for this module. Important that this is not the same as the griptape_nodes logger or else we'll have infinite log events.
 logger = logging.getLogger("griptape_nodes_app")
 console = Console()
@@ -96,6 +90,12 @@ def start_app() -> None:
     Starts the event loop and listens for events from the Nodes API.
     """
     _init_event_listeners()
+
+    griptape_nodes_logger = logging.getLogger("griptape_nodes")
+    # When running as an app, we want to forward all log messages to the event queue so they can be sent to the GUI
+    griptape_nodes_logger.addHandler(EventLogHandler())
+    griptape_nodes_logger.addHandler(RichHandler(show_time=True, show_path=False, markup=True, rich_tracebacks=True))
+    griptape_nodes_logger.setLevel(logging.INFO)
 
     # Listen for any signals to exit the app
     for sig in (signal.SIGINT, signal.SIGTERM):
