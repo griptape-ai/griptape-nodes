0cbff5205881cba76ad2ee6880aabe3e8c709085
Jason Schleifer <jason.schleifer@gmail.com>
2025-06-10 04:59:32 +1200

    update: tools update, new text tasks, updated documentation (#1423)
    * added a summarize text task node
    
    * reverted agent, base_tool to what they were on main
    
    * new nodes - date and time, evaluate text, summarize text, etc
    
    * moved task nodes to the text area
    
    * moved Math node in library
    
    * adding helpful info
    
    * updating the tools
    
    * working on filemanager and adding ability to reorder parameters.
    
    * adding file manager location
    
    * temporarily hid GrlptapeCloudFileManagerDriver as an option
    
    * updated websearch tool
    
    * removed duckduckdgo driver and fixed config settings
    
    * updated tool documentation
    
    * updated docs and organized library
    
    * format
    
    * linter
    
    * linter
    
    * linter
    
    * linter
    
    * linter
    
    * spelling
    
    * fixed process definition
    
    * fixed process
    
    * renamed summarize_text
    
    * fixed the api check to make sure we weren't getting None
    
    * updated .gitignore
    
    * reused reorder_elements code and tested to ensure connections stay

--- libraries/griptape_nodes_library/griptape_nodes_library/tools/calculator_tool.py
+++ libraries/griptape_nodes_library/griptape_nodes_library/tools/calculator_tool.py
@@ -4,6 +4,15 @@ from griptape_nodes_library.tools.base_tool import BaseTool
 
 
 class Calculator(BaseTool):
+    def __init__(self, **kwargs) -> None:
+        super().__init__(**kwargs)
+
+        self.update_tool_info(
+            value="This calculator tool can be given to an agent to help it perform calculations.",
+            title="Calculator Tool",
+        )
+        self.hide_parameter_by_name("off_prompt")
+
     def process(self) -> None:
         off_prompt = self.parameter_values.get("off_prompt", True)
 
