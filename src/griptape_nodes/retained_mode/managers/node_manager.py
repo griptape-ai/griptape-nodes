0467019da78f148d199aa59cb4913aadb74ee4f2
Jason Schleifer <jason.schleifer@gmail.com>
2025-06-18 06:55:35 +1200

    reverted to main

--- src/griptape_nodes/retained_mode/managers/node_manager.py
+++ src/griptape_nodes/retained_mode/managers/node_manager.py
@@ -1202,10 +1202,7 @@ class NodeManager:
 
         return None
 
-    # For C901 (too complex): Need to give customers explicit reasons for failure on each case.
-    # For PLR0911 (too many return statements): Need to give customers explicit reasons for failure on each case.
-    # For PLR0912 (too many local variables): Need to give customers explicit reasons for failure on each case.
-    def on_alter_parameter_details_request(self, request: AlterParameterDetailsRequest) -> ResultPayload:  # noqa: C901, PLR0911, PLR0912
+    def on_alter_parameter_details_request(self, request: AlterParameterDetailsRequest) -> ResultPayload:  # noqa: C901, PLR0911
         node_name = request.node_name
         node = None
 
@@ -1213,37 +1210,34 @@ class NodeManager:
             if not GriptapeNodes.ContextManager().has_current_node():
                 details = f"Attempted to alter details for Parameter '{request.parameter_name}' from node in the Current Context. Failed because there was no such Node."
                 logger.error(details)
+
                 return AlterParameterDetailsResultFailure()
             node = GriptapeNodes.ContextManager().get_current_node()
             node_name = node.name
-        else:
-            try:
-                node = self.get_node_by_name(node_name)
-            except Exception as e:
-                details = f"Failed to get Node '{node_name}': {e}"
+
+        # Does this node exist?
+        if node is None:
+            obj_mgr = GriptapeNodes.ObjectManager()
+            node = obj_mgr.attempt_get_object_by_name_as_type(node_name, BaseNode)
+            if node is None:
+                details = f"Attempted to alter details for Parameter '{request.parameter_name}' from Node '{node_name}', but no such Node was found."
                 logger.error(details)
+
                 return AlterParameterDetailsResultFailure()
 
-        # Handle ParameterGroup
+        # Does the Parameter actually exist on the Node?
+        parameter = node.get_parameter_by_name(request.parameter_name)
         parameter_group = node.get_group_by_name_or_element_id(request.parameter_name)
-        if parameter_group is not None:
+        if parameter is None:
+            parameter_group = node.get_group_by_name_or_element_id(request.parameter_name)
+            if parameter_group is None:
+                details = f"Attempted to alter details for Parameter '{request.parameter_name}' from Node '{node_name}'. Failed because it didn't have a Parameter with that name on it."
+                logger.error(details)
+
+                return AlterParameterDetailsResultFailure()
             if request.ui_options is not None:
                 parameter_group.ui_options = request.ui_options
             return AlterParameterDetailsResultSuccess()
-
-        # Handle ParameterMessage
-        parameter_message = node.get_message_by_name_or_element_id(request.parameter_name)
-        if parameter_message is not None:
-            if request.ui_options is not None:
-                parameter_message.ui_options = request.ui_options
-            return AlterParameterDetailsResultSuccess()
-
-        # Handle Parameter
-        parameter = node.get_parameter_by_name(request.parameter_name)
-        if parameter is None:
-            details = f"Parameter '{request.parameter_name}' not found in Node '{node_name}'."
-            logger.error(details)
-            return AlterParameterDetailsResultFailure()
 
         # Check and handle connections if type was changed
         if request.type is not None or request.input_types is not None or request.output_type is not None:
@@ -1260,7 +1254,6 @@ class NodeManager:
             details = f"Attempted to alter details for Parameter '{request.parameter_name}' from Node '{node_name}'. Could only alter some values because the Parameter was not user-defined (i.e., critical to the Node implementation). Only user-defined Parameters can be totally modified from a Node."
             logger.warning(details)
             return AlterParameterDetailsResultSuccess()
-
         self.modify_key_parameter_fields(request, parameter)
         # This field requires the node as well
         if request.default_value is not None:
@@ -1270,12 +1263,8 @@ class NodeManager:
         details = f"Successfully altered details for Parameter '{request.parameter_name}' from Node '{node_name}'."
         logger.debug(details)
 
-        # If we're not in initial setup, we need to emit an event to update the UI
-        if not request.initial_setup:
-            modified_request = AlterElementEvent(element_details=parameter.to_event(node))
-            EventBus.publish_event(ExecutionGriptapeNodeEvent(ExecutionEvent(payload=modified_request)))
-
-        return AlterParameterDetailsResultSuccess()
+        result = AlterParameterDetailsResultSuccess()
+        return result
 
     # For C901 (too complex): Need to give customers explicit reasons for failure on each case.
     def on_get_parameter_value_request(self, request: GetParameterValueRequest) -> ResultPayload:
