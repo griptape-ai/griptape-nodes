bbc3f28cd1d441306f19ff198bf0341e041aaa81
James Clarendon <james.clarendon@gmail.com>
2025-06-20 09:49:55 -0700

    No longer default to unused templates dir. Learn how to Python lists.

--- src/griptape_nodes/retained_mode/managers/settings.py
+++ src/griptape_nodes/retained_mode/managers/settings.py
@@ -7,11 +7,7 @@ from xdg_base_dirs import xdg_data_home
 
 class AppInitializationComplete(BaseModel):
     libraries_to_register: list[str] = Field(default_factory=list)
-    workflows_to_register: list[str] = Field(
-        default_factory=lambda: [
-            str(xdg_data_home() / "griptape_nodes/workflows/templates/"),
-        ]
-    )
+    workflows_to_register: list[str] = Field(default_factory=list)
 
 
 class AppEvents(BaseModel):
