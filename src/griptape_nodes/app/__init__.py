45f85c2491c536fdd8cf51293ad27f8eaa1194a3
Collin Dutter <collin@griptape.ai>
2025-06-20 09:24:04 -0700

    refactor: remove legacy SSE app code (#1582)

--- src/griptape_nodes/app/__init__.py
+++ src/griptape_nodes/app/__init__.py
@@ -1,10 +1,5 @@
 """App package."""
 
-import os
-
-if os.getenv("GTN_USE_WEBSOCKETS", "true").lower() == "true":
-    from griptape_nodes.app.app_websocket import start_app
-else:
-    from griptape_nodes.app.app import start_app
+from griptape_nodes.app.app import start_app
 
 __all__ = ["start_app"]
