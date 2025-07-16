50ae9a79d3a0fc6b78aa4ee1f4e0dd89be45e0b1
Jason Schleifer <jason.schleifer@gmail.com>
2025-07-16 20:10:27 +1200

    added width and height and updated docs

--- libraries/griptape_nodes_library/griptape_nodes_library/image/display_image.py
+++ libraries/griptape_nodes_library/griptape_nodes_library/image/display_image.py
@@ -1,10 +1,15 @@
+from io import BytesIO
 from typing import Any
+
+import requests
+from griptape.artifacts import ImageArtifact, ImageUrlArtifact
+from PIL import Image
 
 from griptape_nodes.exe_types.core_types import (
     Parameter,
     ParameterMode,
 )
-from griptape_nodes.exe_types.node_types import DataNode
+from griptape_nodes.exe_types.node_types import BaseNode, DataNode
 
 
 class DisplayImage(DataNode):
@@ -28,7 +33,56 @@ class DisplayImage(DataNode):
                 allowed_modes={ParameterMode.INPUT, ParameterMode.OUTPUT, ParameterMode.PROPERTY},
             )
         )
+        self.add_parameter(
+            Parameter(
+                name="width",
+                type="int",
+                default_value=0,
+                tooltip="The width of the image",
+                allowed_modes={ParameterMode.OUTPUT},
+                ui_options={"hide": True},
+            )
+        )
+        self.add_parameter(
+            Parameter(
+                name="height",
+                type="int",
+                default_value=0,
+                tooltip="The height of the image",
+                allowed_modes={ParameterMode.OUTPUT},
+                ui_options={"hide": True},
+            )
+        )
+
+    def after_incoming_connection(
+        self, source_node: BaseNode, source_parameter: Parameter, target_parameter: Parameter
+    ) -> None:
+        image = self.get_parameter_value("image")
+        if image:
+            width, height = self.get_image_dimensions(image)
+            self.parameter_output_values["width"] = width
+            self.parameter_output_values["height"] = height
+        else:
+            self.parameter_output_values["width"] = 0
+            self.parameter_output_values["height"] = 0
+        return super().after_incoming_connection(source_node, source_parameter, target_parameter)
+
+    def get_image_dimensions(self, image: ImageArtifact | ImageUrlArtifact) -> tuple[int, int]:
+        """Get image dimensions from either ImageArtifact or ImageUrlArtifact."""
+        if isinstance(image, ImageArtifact):
+            return image.width, image.height
+        if isinstance(image, ImageUrlArtifact):
+            response = requests.get(image.value, timeout=30)
+            response.raise_for_status()
+            image_data = response.content
+            pil_image = Image.open(BytesIO(image_data))
+            return pil_image.width, pil_image.height
+        return 0, 0
 
     def process(self) -> None:
-        # Simply output the input image
-        self.parameter_output_values["image"] = self.parameter_values.get("image")
+        image = self.get_parameter_value("image")
+        width, height = self.get_image_dimensions(image)
+
+        self.parameter_output_values["image"] = image
+        self.parameter_output_values["width"] = width
+        self.parameter_output_values["height"] = height
