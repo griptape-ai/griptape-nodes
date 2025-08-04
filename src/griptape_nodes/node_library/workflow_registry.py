857e43d448fd28a8e62ae20b5d1bf3a0e3354a4b
Zach Giordano <zgmx22@gmail.com>
2025-08-04 19:46:21 -0400

    Update generated local executor code

--- src/griptape_nodes/node_library/workflow_registry.py
+++ src/griptape_nodes/node_library/workflow_registry.py
@@ -13,7 +13,7 @@ from griptape_nodes.utils.metaclasses import SingletonMeta
 
 
 class WorkflowMetadata(BaseModel):
-    LATEST_SCHEMA_VERSION: ClassVar[str] = "0.6.1"
+    LATEST_SCHEMA_VERSION: ClassVar[str] = "0.6.2"
 
     name: str
     schema_version: str
