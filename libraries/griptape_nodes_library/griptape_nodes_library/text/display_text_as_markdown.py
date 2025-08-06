f7404910d77fb0849ad721fa949b01cf212d6615
Jason Schleifer <jason.schleifer@gmail.com>
2025-08-06 08:35:44 -0700

    linter

--- libraries/griptape_nodes_library/griptape_nodes_library/text/display_text_as_markdown.py
+++ libraries/griptape_nodes_library/griptape_nodes_library/text/display_text_as_markdown.py
@@ -4,13 +4,8 @@ from griptape_nodes_library.text.display_text import DisplayText
 
 
 class DisplayTextAsMarkdown(DisplayText):
-    def __init__(
-        self,
-        name: str,
-        metadata: dict[Any, Any] | None = None,
-        value: str = "",
-    ) -> None:
-        super().__init__(name, metadata)
+    def __init__(self, **kwargs: Any) -> None:
+        super().__init__(**kwargs)
 
         text_param = self.get_parameter_by_name("text")
         if text_param:
