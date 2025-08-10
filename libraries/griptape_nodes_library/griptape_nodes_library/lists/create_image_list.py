073d53d0c638b96a6e9d70469598375590f81e7f
Jason Schleifer <jason.schleifer@gmail.com>
2025-08-10 11:46:57 -0700

    fixed output type

--- libraries/griptape_nodes_library/griptape_nodes_library/lists/create_image_list.py
+++ libraries/griptape_nodes_library/griptape_nodes_library/lists/create_image_list.py
@@ -24,7 +24,7 @@ class CreateImageList(ControlNode):
         self.output = Parameter(
             name="output",
             tooltip="Output list",
-            output_type="list[ImageUrlArtifact]",
+            output_type="list",
             allowed_modes={ParameterMode.OUTPUT},
         )
         self.add_parameter(self.output)
