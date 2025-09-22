a7e800fe7355c4559d8b588e6a4257b662ad0c88
CJ Kindel <cj@kindel.com>
2025-09-22 14:46:17 -0700

    Initial FluxPipeline support

---
+++ libraries/griptape_nodes_advanced_media_library/diffusers_nodes_library/common/nodes/diffusion_pipeline_node.py
@@ -0,0 +1,71 @@
+import logging
+from typing import Any
+
+from diffusers.pipelines.pipeline_utils import DiffusionPipeline
+
+from diffusers_nodes_library.common.parameters.log_parameter import (  # type: ignore[reportMissingImports]
+    LogParameter,  # type: ignore[reportMissingImports]
+)
+from diffusers_nodes_library.common.parameters.diffusion.diffusion_pipeline_parameters import DiffusionPipelineParameters  # type: ignore[reportMissingImports]
+# from diffusers_nodes_library.common.parameters.diffusion.diffusion_pipeline_runtime_parameters import DiffusionPipelineRuntimeParameters  # type: ignore[reportMissingImports]
+from diffusers_nodes_library.pipelines.flux.flux_loras_parameter import FluxLorasParameter
+from griptape_nodes.exe_types.core_types import Parameter
+from griptape_nodes.exe_types.node_types import AsyncResult, ControlNode
+
+logger = logging.getLogger("diffusers_nodes_library")
+
+
+class DiffusionPipelineNode(ControlNode):
+    def __init__(self, **kwargs) -> None:
+        super().__init__(**kwargs)
+        self.pipe_params = DiffusionPipelineParameters(self)
+        self.pipe_params.add_input_parameters()
+        
+        self.loras_params = FluxLorasParameter(self)
+        self.loras_params.add_input_parameters()
+
+        self.log_params = LogParameter(self)
+        self.log_params.add_output_parameters()
+
+    def after_value_set(self, parameter: Parameter, value: Any) -> None:
+        self.pipe_params.after_value_set(parameter, value)
+        self.pipe_params.runtime_parameters.after_value_set(parameter, value)
+
+    def preprocess(self) -> None:
+        self.pipe_params.runtime_parameters.preprocess()
+        self.log_params.clear_logs()
+
+    def process(self) -> AsyncResult | None:
+        yield lambda: self._process()
+
+    def _process(self) -> AsyncResult | None:
+        self.preprocess()
+        self.pipe_params.runtime_parameters.publish_output_image_preview_placeholder()
+
+        with (
+            self.log_params.append_profile_to_logs("Configuring FLUX loras"),
+            self.log_params.append_logs_to_logs(logger),
+        ):
+            self.loras_params.configure_loras(self.pipe_params.pipeline)
+
+        num_inference_steps = self.pipe_params.runtime_parameters.get_num_inference_steps()
+
+        def callback_on_step_end(
+            pipe: DiffusionPipeline,
+            i: int,
+            _t: Any,
+            callback_kwargs: dict,
+        ) -> dict:
+            if i < num_inference_steps - 1:
+                self.pipe_params.runtime_parameters.publish_output_image_preview_latents(self.pipe_params.pipeline, callback_kwargs["latents"])
+                self.log_params.append_to_logs(f"Starting inference step {i + 2} of {num_inference_steps}...\n")
+            return {}
+
+        self.log_params.append_to_logs(f"Starting inference step 1 of {num_inference_steps}...\n")
+        output_image_pil = self.pipe_params.pipeline(  # type: ignore[reportCallIssue]
+            **self.pipe_params.runtime_parameters.get_pipe_kwargs(),
+            output_type="pil",
+            callback_on_step_end=callback_on_step_end,
+        ).images[0]
+        self.pipe_params.runtime_parameters.publish_output_image(output_image_pil)
+        self.log_params.append_to_logs("Done.\n")
---
+++ libraries/griptape_nodes_advanced_media_library/diffusers_nodes_library/common/parameters/diffusion/diffusion_pipeline_parameters.py
@@ -0,0 +1,52 @@
+import logging
+from typing import Any
+
+from diffusers.pipelines.pipeline_utils import DiffusionPipeline
+from diffusers.pipelines.flux.pipeline_flux import FluxPipeline
+from diffusers.pipelines.flux.pipeline_flux_fill import FluxFillPipeline
+
+from diffusers_nodes_library.common.parameters.diffusion.flux.flux_pipeline_runtime_parameters import FluxPipelineRuntimeParameters
+from diffusers_nodes_library.common.parameters.diffusion.diffusion_pipeline_runtime_parameters import DiffusionPipelineRuntimeParameters
+from diffusers_nodes_library.common.utils.huggingface_utils import model_cache
+
+from griptape_nodes.exe_types.node_types import BaseNode
+from griptape_nodes.exe_types.core_types import Parameter
+
+
+
+logger = logging.getLogger("diffusers_nodes_library")
+
+
+class DiffusionPipelineParameters:
+    def __init__(self, node: BaseNode):
+        self._node: BaseNode = node
+        self._runtime_parameters: DiffusionPipelineRuntimeParameters | None = None
+
+    def add_input_parameters(self) -> None:
+        # TODO: Implement with Resource Manager integration - https://github.com/griptape-ai/griptape-nodes/issues/2237
+        pass
+
+    def after_value_set(self, parameter: Parameter, value: Any) -> None:
+        if parameter.name != "diffusion_pipeline":
+            return
+        
+        if isinstance(self.pipeline, FluxPipeline):
+            self._runtime_parameters = FluxPipelineRuntimeParameters(self._node)
+        # elif isinstance(self.pipeline, FluxFillPipeline):
+        #     self._runtime_parameters = FluxFillPipelineRuntimeParameters(self._node)
+        else:
+                raise ValueError(f"Unsupported pipeline type: {type(self.pipeline)}")
+
+        self._runtime_parameters.add_input_parameters()
+        self._runtime_parameters.add_output_parameters()
+
+    @property
+    def pipeline(self) -> DiffusionPipeline:
+        # TODO: Implement with Resource Manager integration - https://github.com/griptape-ai/griptape-nodes/issues/2237
+        return model_cache.pipeline
+    
+    @property
+    def runtime_parameters(self) -> DiffusionPipelineRuntimeParameters:
+        if self._runtime_parameters is None:
+            raise ValueError("Runtime parameters not initialized. Ensure diffusion_pipeline parameter is set.")
+        return self._runtime_parameters
