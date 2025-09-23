87cf94930ff4f6e9e31bb46948db6b11a0ce076d
Collin Dutter <collin@griptape.ai>
2025-09-23 15:41:25 -0700

    feat: add support for 6 additional diffusion providers to DiffusionPipelineBuilderParameters

---
+++ libraries/griptape_nodes_advanced_media_library/diffusers_nodes_library/common/parameters/diffusion/allegro/allegro_pipeline_type_parameters.py
@@ -0,0 +1,42 @@
+import logging
+
+from diffusers_nodes_library.common.parameters.diffusion.diffusion_pipeline_type_parameters import (
+    DiffusionPipelineTypeParameters,
+)
+from griptape_nodes.exe_types.node_types import BaseNode
+from libraries.griptape_nodes_advanced_media_library.diffusers_nodes_library.common.parameters.diffusion.allegro.allegro_pipeline_type_allegro_pipeline_parameters import (
+    AllegroPipelineTypeAllegroPipelineParameters,
+)
+from libraries.griptape_nodes_advanced_media_library.diffusers_nodes_library.common.parameters.diffusion.diffusion_pipeline_type_pipeline_parameters import (
+    DiffusionPipelineTypePipelineParameters,
+)
+
+logger = logging.getLogger("diffusers_nodes_library")
+
+
+class AllegroPipelineTypeParameters(DiffusionPipelineTypeParameters):
+    def __init__(self, node: BaseNode):
+        super().__init__(node)
+        self._pipeline_type_pipeline_params: DiffusionPipelineTypePipelineParameters
+        self.set_pipeline_type_pipeline_params(self.pipeline_types[0])
+
+    @property
+    def pipeline_types(self) -> list[str]:
+        return ["AllegroPipeline"]
+
+    def set_pipeline_type_pipeline_params(self, pipeline_type: str) -> None:
+        match pipeline_type:
+            case "AllegroPipeline":
+                self._pipeline_type_pipeline_params = AllegroPipelineTypeAllegroPipelineParameters(self._node)
+            case _:
+                msg = f"Unsupported pipeline type: {pipeline_type}"
+                logger.error(msg)
+                raise ValueError(msg)
+
+    @property
+    def pipeline_type_pipeline_params(self) -> DiffusionPipelineTypePipelineParameters:
+        if self._pipeline_type_pipeline_params is None:
+            msg = "Pipeline type builder parameters not initialized. Ensure provider parameter is set."
+            logger.error(msg)
+            raise ValueError(msg)
+        return self._pipeline_type_pipeline_params
---
+++ libraries/griptape_nodes_advanced_media_library/diffusers_nodes_library/common/parameters/diffusion/audioldm/audioldm_pipeline_type_parameters.py
@@ -0,0 +1,42 @@
+import logging
+
+from diffusers_nodes_library.common.parameters.diffusion.diffusion_pipeline_type_parameters import (
+    DiffusionPipelineTypeParameters,
+)
+from griptape_nodes.exe_types.node_types import BaseNode
+from libraries.griptape_nodes_advanced_media_library.diffusers_nodes_library.common.parameters.diffusion.audioldm.audioldm_pipeline_type_audioldm_pipeline_parameters import (
+    AudioldmPipelineTypeAudioldmPipelineParameters,
+)
+from libraries.griptape_nodes_advanced_media_library.diffusers_nodes_library.common.parameters.diffusion.diffusion_pipeline_type_pipeline_parameters import (
+    DiffusionPipelineTypePipelineParameters,
+)
+
+logger = logging.getLogger("diffusers_nodes_library")
+
+
+class AudioldmPipelineTypeParameters(DiffusionPipelineTypeParameters):
+    def __init__(self, node: BaseNode):
+        super().__init__(node)
+        self._pipeline_type_pipeline_params: DiffusionPipelineTypePipelineParameters
+        self.set_pipeline_type_pipeline_params(self.pipeline_types[0])
+
+    @property
+    def pipeline_types(self) -> list[str]:
+        return ["AudioLDMPipeline"]
+
+    def set_pipeline_type_pipeline_params(self, pipeline_type: str) -> None:
+        match pipeline_type:
+            case "AudioLDMPipeline":
+                self._pipeline_type_pipeline_params = AudioldmPipelineTypeAudioldmPipelineParameters(self._node)
+            case _:
+                msg = f"Unsupported pipeline type: {pipeline_type}"
+                logger.error(msg)
+                raise ValueError(msg)
+
+    @property
+    def pipeline_type_pipeline_params(self) -> DiffusionPipelineTypePipelineParameters:
+        if self._pipeline_type_pipeline_params is None:
+            msg = "Pipeline type builder parameters not initialized. Ensure provider parameter is set."
+            logger.error(msg)
+            raise ValueError(msg)
+        return self._pipeline_type_pipeline_params
