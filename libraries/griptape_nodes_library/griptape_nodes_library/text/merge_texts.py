e66379d034efa8a29367e9cbf06cf59a03937b25
Jason Schleifer <jason.schleifer@gmail.com>
2025-09-25 12:09:56 +1200

    updated merge_text to handle other input types

--- libraries/griptape_nodes_library/griptape_nodes_library/text/merge_texts.py
+++ libraries/griptape_nodes_library/griptape_nodes_library/text/merge_texts.py
@@ -20,7 +20,7 @@ class MergeTexts(DataNode):
             Parameter(
                 name="input_1",
                 allowed_modes={ParameterMode.INPUT, ParameterMode.PROPERTY},
-                input_types=["str"],
+                input_types=["str", "int", "float", "bool"],
                 tooltip="Text inputs to merge together.",
             )
         )
@@ -28,7 +28,7 @@ class MergeTexts(DataNode):
             Parameter(
                 name="input_2",
                 allowed_modes={ParameterMode.INPUT, ParameterMode.PROPERTY},
-                input_types=["str"],
+                input_types=["str", "int", "float", "bool"],
                 tooltip="Text inputs to merge together.",
             )
         )
@@ -36,7 +36,7 @@ class MergeTexts(DataNode):
             Parameter(
                 name="input_3",
                 allowed_modes={ParameterMode.INPUT, ParameterMode.PROPERTY},
-                input_types=["str"],
+                input_types=["str", "int", "float", "bool"],
                 tooltip="Text inputs to merge together.",
             )
         )
@@ -44,7 +44,7 @@ class MergeTexts(DataNode):
             Parameter(
                 name="input_4",
                 allowed_modes={ParameterMode.INPUT, ParameterMode.PROPERTY},
-                input_types=["str"],
+                input_types=["str", "int", "float", "bool"],
                 tooltip="Text inputs to merge together.",
             )
         )
@@ -69,10 +69,16 @@ class MergeTexts(DataNode):
                 output_type="str",
                 default_value="",
                 tooltip="The merged text result.",
+                ui_options={"multiline": True},
             )
         )
 
-    def process(self) -> None:
+    def after_value_set(self, parameter: Parameter, value: Any) -> None:
+        if parameter.name in ["input_1", "input_2", "input_3", "input_4", "merge_string"]:
+            self._merge_texts()
+        return super().after_value_set(parameter, value)
+
+    def _merge_texts(self) -> None:
         # Get the list of input texts
         input_1 = self.parameter_values.get("input_1", None)
         input_2 = self.parameter_values.get("input_2", None)
@@ -82,13 +88,17 @@ class MergeTexts(DataNode):
         # Get the separator string and replace \n with actual newlines
         separator = self.parameter_values.get("merge_string", "\\n\\n").replace("\\n", "\n")
 
-        # Create a list of input texts if they aren't none
-        input_texts = [input_1, input_2, input_3, input_4]
-        # Filter out None values from the list
-        input_texts = [text for text in input_texts if text is not None]
+        # Create a list of input texts, filtering out None values and converting to strings
+        input_texts = [str(text) for text in [input_1, input_2, input_3, input_4] if text is not None]
 
         # Join all the inputs with the separator
         merged_text = separator.join(input_texts).strip()
 
         # Set the output
+        self.set_parameter_value("output", merged_text)
+        self.publish_update_to_parameter("output", merged_text)
         self.parameter_output_values["output"] = merged_text
+
+    def process(self) -> None:
+        # Merge the texts
+        self._merge_texts()
