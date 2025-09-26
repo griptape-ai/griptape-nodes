775cb9c87b2444ef5f54481fe1e069c49f3fec07
CJ Kindel <cj@kindel.com>
2025-09-26 11:24:42 -0700

    fix qwen img2img

--- libraries/griptape_nodes_advanced_media_library/diffusers_nodes_library/common/parameters/diffusion/qwen/qwen_upscale_pipeline_runtime_parameters.py
+++ libraries/griptape_nodes_advanced_media_library/diffusers_nodes_library/common/parameters/diffusion/qwen/qwen_upscale_pipeline_runtime_parameters.py
@@ -13,3 +13,12 @@ class QwenUpscalePipelineRuntimeParameters(UpscalePipelineRuntimeParameters):
 
         self._node.hide_parameter_by_name("prompt_2")
         self._node.hide_parameter_by_name("negative_prompt_2")
+
+    def _get_pipe_kwargs(self) -> dict:
+        return {
+            "prompt": self._node.get_parameter_value("prompt"),
+            "negative_prompt": self._node.get_parameter_value("negative_prompt"),
+            "guidance_scale": self._node.get_parameter_value("guidance_scale"),
+            "image": self.get_image_pil(),
+            "strength": self._node.get_parameter_value("strength"),
+        }
