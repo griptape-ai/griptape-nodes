45645f7490444dcb3eea6958f61b2ddcfbc152c1
James Clarendon <james.clarendon@gmail.com>
2025-09-26 14:47:41 -0700

    add node-supplied imports into workflow saves

--- src/griptape_nodes/retained_mode/managers/workflow_manager.py
+++ src/griptape_nodes/retained_mode/managers/workflow_manager.py
@@ -1395,7 +1395,7 @@ class WorkflowManager:
             image=image_path,
         )
 
-    def _generate_workflow_file_content(  # noqa: PLR0912, C901
+    def _generate_workflow_file_content(  # noqa: PLR0912, PLR0915, C901
         self,
         serialized_flow_commands: SerializedFlowCommands,
         workflow_metadata: WorkflowMetadata,
@@ -1409,6 +1409,13 @@ class WorkflowManager:
 
         import_recorder = ImportRecorder()
         import_recorder.add_from_import("griptape_nodes.retained_mode.griptape_nodes", "GriptapeNodes")
+
+        # Add imports from node dependencies
+        for import_dep in serialized_flow_commands.node_dependencies.imports:
+            if import_dep.class_name:
+                import_recorder.add_from_import(import_dep.module, import_dep.class_name)
+            else:
+                import_recorder.add_import(import_dep.module)
 
         ast_container = ASTContainer()
 
