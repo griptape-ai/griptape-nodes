griptape-ai/griptape-nodes

commit 112536a3236847a3d8b2de5575edc9b9a484c2d2

Feat/Add Loras before quantization (#2429)
* move loras to builder

* sort loras to bottom

* fuse lora weights

* english

* format

libraries/griptape_nodes_advanced_media_library/diffusers_nodes_library/common/parameters/diffusion/diffusion_pipeline_type_parameters.py
@@ -64,7 +64,7 @@ class DiffusionPipelineTypeParameters(ABC):
 
         # Add all other parameters that aren't already positioned or at the end
         hf_param_names = HuggingFacePipelineParameter.get_hf_pipeline_parameter_names()
-        end_params = {*hf_param_names, "logs"}
+        end_params = {*hf_param_names, "loras", "logs"}
         positioned_params = {"pipeline", "provider", "pipeline_type"}
 
         sorted_parameters.extend(

libraries/griptape_nodes_advanced_media_library/diffusers_nodes_library/common/nodes/diffusion_pipeline_builder_node.py
@@ -10,6 +10,7 @@ from diffusers_nodes_library.common.parameters.diffusion.builder_parameters impo
 from diffusers_nodes_library.common.parameters.huggingface_pipeline_parameter import HuggingFacePipelineParameter
 from diffusers_nodes_library.common.parameters.log_parameter import LogParameter
 from diffusers_nodes_library.common.utils.huggingface_utils import model_cache
+from diffusers_nodes_library.common.utils.lora_utils import FluxLorasParameter
 from diffusers_nodes_library.common.utils.pipeline_utils import optimize_diffusion_pipeline
 from griptape_nodes.exe_types.core_types import Parameter
 from griptape_nodes.exe_types.node_types import ControlNode
@@ -30,6 +31,9 @@ class DiffusionPipelineBuilderNode(ControlNode):
         self.params.add_output_parameters()
         self.params.add_input_parameters()
         self.huggingface_pipeline_params.add_input_parameters()
+
+        self.loras_params = FluxLorasParameter(self)
+        self.loras_params.add_input_parameters()
 
         self.log_params.add_output_parameters()
 
@@ -52,6 +56,7 @@ class DiffusionPipelineBuilderNode(ControlNode):
         """Generate a hash for the current configuration to use as cache key."""
         config_data = {
             **self.params.get_config_kwargs(),
+            **self.loras_params.get_loras(),
             "torch_dtype": "bfloat16",  # Currently hardcoded
         }
 
@@ -132,6 +137,9 @@ class DiffusionPipelineBuilderNode(ControlNode):
 
         with self.log_params.append_profile_to_logs("Loading pipeline"):
             pipe = self.params.pipeline_type_parameters.pipeline_type_pipeline_params.build_pipeline()
+
+        with self.log_params.append_profile_to_logs("Configuring FLUX loras"):
+            self.loras_params.configure_loras(pipe)
 
         with self.log_params.append_profile_to_logs("Applying optimizations"):
             optimization_kwargs = self.huggingface_pipeline_params.get_hf_pipeline_parameters()

libraries/griptape_nodes_advanced_media_library/diffusers_nodes_library/common/nodes/diffusion_pipeline_runtime_node.py
@@ -11,7 +11,6 @@ from diffusers_nodes_library.common.parameters.log_parameter import (  # type: i
     LogParameter,  # type: ignore[reportMissingImports]
 )
 from diffusers_nodes_library.common.utils.huggingface_utils import model_cache
-from diffusers_nodes_library.pipelines.flux.flux_loras_parameter import FluxLorasParameter
 from griptape_nodes.exe_types.core_types import Parameter
 from griptape_nodes.exe_types.node_types import BaseNode, ControlNode
 
@@ -24,9 +23,6 @@ class DiffusionPipelineRuntimeNode(ControlNode):
         super().__init__(**kwargs)
         self.pipe_params = DiffusionPipelineParameters(self)
         self.pipe_params.add_input_parameters()
-
-        self.loras_params = FluxLorasParameter(self)
-        self.loras_params.add_input_parameters()
 
         self.log_params = LogParameter(self)
         self.log_params.add_output_parameters()
@@ -68,9 +64,9 @@ class DiffusionPipelineRuntimeNode(ControlNode):
         if did_pipeline_change:
             sorted_parameters = ["pipeline"]
             sorted_parameters.extend(
-                [param.name for param in self.parameters if param.name not in ["pipeline", "loras", "logs"]]
+                [param.name for param in self.parameters if param.name not in ["pipeline", "logs"]]
             )
-            sorted_parameters.extend(["loras", "logs"])
+            sorted_parameters.extend(["logs"])
             self.reorder_elements(sorted_parameters)
 
         self.pipe_params.runtime_parameters.after_value_set(parameter, value)
@@ -120,11 +116,5 @@ class DiffusionPipelineRuntimeNode(ControlNode):
         self.preprocess()
         self.pipe_params.runtime_parameters.publish_output_image_preview_placeholder()
         pipe = self._get_pipeline()
-
-        with (
-            self.log_params.append_profile_to_logs("Configuring FLUX loras"),
-            self.log_params.append_logs_to_logs(logger),
-        ):
-            self.loras_params.configure_loras(pipe)
 
         await asyncio.to_thread(self.pipe_params.runtime_parameters.process_pipeline, pipe)

libraries/griptape_nodes_advanced_media_library/diffusers_nodes_library/common/utils/lora_utils.py
@@ -3,10 +3,7 @@ import logging
 from typing import Any
 
 import safetensors  # type: ignore[reportMissingImports]
-import torch  # type: ignore[reportMissingImports]
-import torch.nn.functional  # type: ignore[reportMissingImports]
 
-from diffusers_nodes_library.common.utils.torch_utils import get_best_device  # type: ignore[reportMissingImports]
 from griptape_nodes.exe_types.core_types import ParameterList, ParameterMode
 from griptape_nodes.exe_types.node_types import BaseNode
 
@@ -34,15 +31,19 @@ class FluxLorasParameter:
         """Returns a unique name for an adapter given its model path."""
         return hashlib.sha256(model_path.encode("utf-8")).hexdigest()
 
-    def configure_loras(self, pipe: Any) -> None:
+    def get_loras(self) -> dict[str, float]:
         loras_list = self._node.get_parameter_value(self._loras_parameter_name) or []
 
         loras = {}
         for lora in loras_list:
             loras.update(lora)
 
+        return loras
+
+    def configure_loras(self, pipe: Any) -> None:
+        loras = self.get_loras()
+
         if not loras:
-            pipe.disable_lora()
             return
 
         lora_by_name = {
@@ -65,31 +66,13 @@ class FluxLorasParameter:
             state_dict = safetensors.torch.load_file(lora_path)  # type: ignore[reportAttributeAccessIssue]
             pipe.load_lora_weights(state_dict, adapter_name=item["name"])
 
-        if loras_to_load and get_best_device(quiet=True) == torch.device("cuda"):
-            # If we loaded any loras, make sure the layers have been casted
-            # to bfloat16. For cuda, we enable the layerwise caching on the
-            # transformer, but this isn't applied to the linear layers added
-            # by the loras. Those are loaded as fp8, and cuda doesn't have
-            # all the right matrix operation kernels for fp8, so we need to
-            # cast to bfloat16. That's fine because there are a lot less lora
-            # weights when compared to the original transformer (so it won't
-            # take up that much space on the device).
-            # TODO: https://github.com/griptape-ai/griptape-nodes/issues/849
-            logger.info("converting all lora_A and lora_B layers to bfloat16")
-            lora_modules = [
-                module
-                for name, module in pipe.transformer.named_modules()
-                if hasattr(module, "lora_A") or hasattr(module, "lora_B")
-            ]
-
-            logger.info("Converting %d lora related layers to bfloat16", len(lora_modules))
-            for module in lora_modules:
-                module.to(dtype=torch.bfloat16)
-
         # Use them with given weights.
         adapter_names = [v["name"] for v in lora_by_name.values()]
         adapter_weights = [v["weight"] for v in lora_by_name.values()]
         msg = f"Using adapter_names with weights:\n{adapter_names=}\n{adapter_weights=}"
         logger.info(msg)
         pipe.set_adapters(adapter_names=adapter_names, adapter_weights=adapter_weights)
-        pipe.enable_lora()
+
+        logger.info("Fusing lora weights with diffusion model.")
+        pipe.fuse_lora(adapter_names=adapter_names, lora_scale=1.0)
+        pipe.unload_lora_weights()

libraries/griptape_nodes_advanced_media_library/diffusers_nodes_library/common/utils/pipeline_utils.py
@@ -252,7 +252,7 @@ def _manual_optimize_diffusion_pipeline(  # noqa: C901 PLR0912 PLR0913
 def optimize_diffusion_pipeline(  # noqa: PLR0913
     pipe: DiffusionPipeline,
     *,
-    memory_optimization_strategy: str = "Automatic",
+    memory_optimization_strategy: str = "Manual",
     attention_slicing: bool = False,
     vae_slicing: bool = False,
     transformer_layerwise_casting: bool = False,

libraries/griptape_nodes_advanced_media_library/diffusers_nodes_library/pipelines/flux/controlnet/union_flux_control_net_pipeline.py
@@ -11,12 +11,12 @@ from diffusers_nodes_library.common.parameters.log_parameter import (  # type: i
     LogParameter,  # type: ignore[reportMissingImports]
 )
 from diffusers_nodes_library.common.utils.huggingface_utils import model_cache  # type: ignore[reportMissingImports]
+from diffusers_nodes_library.common.utils.lora_utils import (
+    FluxLorasParameter,  # type: ignore[reportMissingImports]
+)
 from diffusers_nodes_library.pipelines.flux.controlnet.union_one_flux_control_net_model import (
     UnionOneFluxControlNetParameters,
 )  # type: ignore[reportMissingImports]
-from diffusers_nodes_library.pipelines.flux.flux_loras_parameter import (
-    FluxLorasParameter,  # type: ignore[reportMissingImports]
-)
 from diffusers_nodes_library.pipelines.flux.flux_pipeline_memory_footprint import safe_optimize_flux_pipeline
 from diffusers_nodes_library.pipelines.flux.flux_pipeline_parameters import (
     FluxPipelineParameters,  # type: ignore[reportMissingImports]

libraries/griptape_nodes_advanced_media_library/diffusers_nodes_library/pipelines/flux/controlnet/union_pro_flux_control_net_pipeline.py
@@ -9,10 +9,10 @@ from diffusers_nodes_library.common.parameters.log_parameter import (  # type: i
     LogParameter,  # type: ignore[reportMissingImports]
 )
 from diffusers_nodes_library.common.utils.huggingface_utils import model_cache  # type: ignore[reportMissingImports]
+from diffusers_nodes_library.common.utils.lora_utils import FluxLorasParameter
 from diffusers_nodes_library.pipelines.flux.controlnet.union_one_flux_control_net_model import (
     UnionOneFluxControlNetParameters,
 )  # type: ignore[reportMissingImports]
-from diffusers_nodes_library.pipelines.flux.flux_loras_parameter import FluxLorasParameter
 from diffusers_nodes_library.pipelines.flux.flux_pipeline_memory_footprint import safe_optimize_flux_pipeline
 from diffusers_nodes_library.pipelines.flux.flux_pipeline_parameters import (
     FluxPipelineParameters,  # type: ignore[reportMissingImports]

libraries/griptape_nodes_advanced_media_library/diffusers_nodes_library/pipelines/flux/controlnet/union_pro_two_flux_control_net_pipeline.py
@@ -7,12 +7,12 @@ import torch  # type: ignore[reportMissingImports]
 from diffusers_nodes_library.common.parameters.huggingface_repo_parameter import HuggingFaceRepoParameter
 from diffusers_nodes_library.common.parameters.log_parameter import LogParameter  # type: ignore[reportMissingImports]
 from diffusers_nodes_library.common.utils.huggingface_utils import model_cache  # type: ignore[reportMissingImports]
+from diffusers_nodes_library.common.utils.lora_utils import (
+    FluxLorasParameter,  # type: ignore[reportMissingImports]
+)
 from diffusers_nodes_library.pipelines.flux.controlnet.union_two_flux_control_net_parameters import (
     UnionTwoFluxControlNetParameters,
 )  # type: ignore[reportMissingImports]
-from diffusers_nodes_library.pipelines.flux.flux_loras_parameter import (
-    FluxLorasParameter,  # type: ignore[reportMissingImports]
-)
 from diffusers_nodes_library.pipelines.flux.flux_pipeline_memory_footprint import safe_optimize_flux_pipeline
 from diffusers_nodes_library.pipelines.flux.flux_pipeline_parameters import (
     FluxPipelineParameters,  # type: ignore[reportMissingImports]

commit 7946c2feb55f07d0a7c0d69c0d119a17a94a5cf8

fix: properly position "loras" parameter
Fixes regression introduced in 112536a32368

libraries/griptape_nodes_advanced_media_library/diffusers_nodes_library/common/parameters/diffusion/diffusion_pipeline_type_parameters.py
@@ -76,7 +76,7 @@ class DiffusionPipelineTypeParameters(ABC):
         )
 
         # Add end parameters
-        sorted_parameters.extend([*hf_param_names, "logs"])
+        sorted_parameters.extend([*hf_param_names, "loras", "logs"])
 
         self._node.reorder_elements(sorted_parameters)
 