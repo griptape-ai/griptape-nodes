6690100676dd87c237dbf3d120329127985db960
CJ Kindel <cj@kindel.com>
2025-09-30 22:39:37 -0700

    Use legacy FluxLorasParamters for legacy nodes

--- libraries/griptape_nodes_advanced_media_library/diffusers_nodes_library/pipelines/flux/flux_kontext_pipeline.py
+++ libraries/griptape_nodes_advanced_media_library/diffusers_nodes_library/pipelines/flux/flux_kontext_pipeline.py
@@ -8,7 +8,7 @@ from diffusers_nodes_library.common.parameters.log_parameter import (  # type: i
     LogParameter,  # type: ignore[reportMissingImports]
 )
 from diffusers_nodes_library.common.utils.huggingface_utils import model_cache  # type: ignore[reportMissingImports]
-from diffusers_nodes_library.common.utils.lora_utils import FluxLorasParameter
+from diffusers_nodes_library.pipelines.flux.flux_loras_parameter import FluxLorasParameter
 from diffusers_nodes_library.pipelines.flux.flux_kontext_pipeline_parameters import (
     FluxKontextPipelineParameters,  # type: ignore[reportMissingImports]
 )
