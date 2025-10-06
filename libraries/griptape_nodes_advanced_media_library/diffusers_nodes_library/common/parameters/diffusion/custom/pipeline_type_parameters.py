97fbe36ed39af77c17e785be23114ba89cab6316
CJ Kindel <cj@kindel.com>
2025-10-06 16:07:41 -0700

    search pipeline types for custom

--- libraries/griptape_nodes_advanced_media_library/diffusers_nodes_library/common/parameters/diffusion/custom/pipeline_type_parameters.py
+++ libraries/griptape_nodes_advanced_media_library/diffusers_nodes_library/common/parameters/diffusion/custom/pipeline_type_parameters.py
@@ -20,7 +20,8 @@ from diffusers_nodes_library.common.parameters.diffusion.wan.pipeline_type_param
 from diffusers_nodes_library.common.parameters.diffusion.wuerstchen.pipeline_type_parameters import (
     WuerstchenPipelineTypeDict,
 )
-from griptape_nodes.exe_types.core_types import ParameterMessage
+from griptape_nodes.exe_types.core_types import Parameter, ParameterMessage, ParameterMode
+from griptape_nodes.traits.options import Options
 
 logger = logging.getLogger("diffusers_nodes_library")
 
@@ -42,6 +43,14 @@ class CustomPipelineTypeParameters(DiffusionPipelineTypeParameters):
     def pipeline_type_dict(self) -> dict[str, type[DiffusionPipelineTypePipelineParameters]]:
         return AllPipelineTypes
 
+    def set_pipeline_type_pipeline_params(self, pipeline_type: str) -> None:
+        try:
+            self._pipeline_type_pipeline_params = self.pipeline_type_dict[pipeline_type](self._node, list_all_models=True)
+        except KeyError:
+            msg = f"Unsupported pipeline type: {pipeline_type}"
+            logger.error(msg)
+            raise ValueError(msg)
+
     def add_input_parameters(self) -> None:
         self._node.add_node_element(
             ParameterMessage(
@@ -51,16 +60,18 @@ class CustomPipelineTypeParameters(DiffusionPipelineTypeParameters):
                 value="In 'Custom' mode all compatibility guardrails are off. Ensure you are selecting compatible pipeline types and models.",
             )
         )
-        super().add_input_parameters()
-
-    def set_pipeline_type_pipeline_params(self, pipeline_type: str) -> None:
-        try:
-            self._pipeline_type_pipeline_params = self.pipeline_type_dict[pipeline_type](self._node, list_all_models=True)
-        except KeyError:
-            msg = f"Unsupported pipeline type: {pipeline_type}"
-            logger.error(msg)
-            raise ValueError(msg)
+        self._node.add_parameter(
+            Parameter(
+                name="pipeline_type",
+                type="str",
+                traits={Options(choices=self.pipeline_types)},
+                tooltip="Type of diffusion pipeline to build",
+                allowed_modes={ParameterMode.PROPERTY},
+                ui_options={"show_search": True},
+            )
+        )
 
     def remove_input_parameters(self) -> None:
         self._node.remove_parameter_element_by_name("custom_pipeline_type_parameter_notice")
-        return super().remove_input_parameters()
+        self._node.remove_parameter_element_by_name("pipeline_type")
+        self.pipeline_type_pipeline_params.remove_input_parameters()
