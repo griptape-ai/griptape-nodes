b328873cae3b4ec2dee4742ae411da7bbd1d0d87
Collin Dutter <collin@griptape.ai>
2025-10-07 14:53:15 -0700

    fix: properly remove flux model parameters

--- libraries/griptape_nodes_advanced_media_library/diffusers_nodes_library/common/parameters/diffusion/flux/flux_parameters.py
+++ libraries/griptape_nodes_advanced_media_library/diffusers_nodes_library/common/parameters/diffusion/flux/flux_parameters.py
@@ -51,9 +51,9 @@ class FluxPipelineParameters(DiffusionPipelineTypePipelineParameters):
         self._text_encoder_2_repo_parameter.add_input_parameters()
 
     def remove_input_parameters(self) -> None:
-        self._node.remove_parameter_element_by_name("model")
-        self._node.remove_parameter_element_by_name("text_encoder")
-        self._node.remove_parameter_element_by_name("text_encoder_2")
+        self._model_repo_parameter.remove_input_parameters()
+        self._text_encoder_repo_parameter.remove_input_parameters()
+        self._text_encoder_2_repo_parameter.remove_input_parameters()
 
     def get_config_kwargs(self) -> dict:
         return {
