c20fbf00807b73e509c026164a89cfe401a42fe0
CJ Kindel <cjkindel@users.noreply.github.com>
2025-10-20 09:32:28 -0700

    migrate common/parameters to exe_types (#2715)
    * migrate common/parameters to exe_types
    
    * delete unused files
    
    * double init
    
    * format

--- libraries/griptape_nodes_advanced_media_library/diffusers_nodes_library/pipelines/stable_diffusion_ae/stable_diffusion_ae_pipeline.py
+++ libraries/griptape_nodes_advanced_media_library/diffusers_nodes_library/pipelines/stable_diffusion_ae/stable_diffusion_ae_pipeline.py
@@ -4,9 +4,6 @@ from typing import Any
 import diffusers  # type: ignore[reportMissingImports]
 import torch  # type: ignore[reportMissingImports]
 
-from diffusers_nodes_library.common.parameters.log_parameter import (  # type: ignore[reportMissingImports]
-    LogParameter,  # type: ignore[reportMissingImports]
-)
 from diffusers_nodes_library.common.utils.huggingface_utils import model_cache  # type: ignore[reportMissingImports]
 from diffusers_nodes_library.pipelines.stable_diffusion_ae.stable_diffusion_ae_memory_footprint import (
     optimize_stable_diffusion_attend_and_excite_pipeline_memory_footprint,  # type: ignore[reportMissingImports]
@@ -16,6 +13,7 @@ from diffusers_nodes_library.pipelines.stable_diffusion_ae.stable_diffusion_ae_p
 )
 from griptape_nodes.exe_types.core_types import Parameter
 from griptape_nodes.exe_types.node_types import AsyncResult, ControlNode
+from griptape_nodes.exe_types.param_components.log_parameter import LogParameter
 
 logger = logging.getLogger("diffusers_nodes_library")
 
