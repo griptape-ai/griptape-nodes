0f229f53851b3afbddad958e51a75c594b62d16a
CJ Kindel <cjkindel@users.noreply.github.com>
2025-11-18 09:27:11 -0800

    Use original model author's HF repos for upscale models (#3195)
    * Use griptape owned upscale models
    
    * dashes are better
    
    * Use Kim2091's models

--- libraries/griptape_nodes_advanced_media_library/diffusers_nodes_library/common/parameters/diffusion/common/upscale_runtime_parameters.py
+++ libraries/griptape_nodes_advanced_media_library/diffusers_nodes_library/common/parameters/diffusion/common/upscale_runtime_parameters.py
@@ -41,8 +41,10 @@ class UpscalePipelineRuntimeParameters(DiffusionPipelineRuntimeParameters, ABC):
         self._upscale_model_repo_parameter = HuggingFaceRepoFileParameter(
             self._node,
             repo_files=[
-                ("skbhadra/ClearRealityV1", "4x-ClearRealityV1.pth"),
-                ("lokCX/4x-Ultrasharp", "4x-Ultrasharp.pth"),
+                ("Kim2091/ClearRealityV1", "4x-ClearRealityV1.pth"),
+                ("Kim2091/UltraSharp", "4x-UltraSharp.pth"),
+                ("Kim2091/UltraSharpV2", "4x-UltraSharpV2.pth"),
+                ("Kim2091/AnimeSharp", "4x-AnimeSharp.pth"),
             ],
             parameter_name="upscale_model",
         )
