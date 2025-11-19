18c63be78f36c883a57d152a6ac6de119bd80682
dylanholmes <4370153+dylanholmes@users.noreply.github.com>
2025-11-19 23:08:50 +0100

    fixes

--- libraries/griptape_nodes_advanced_media_library/diffusers_nodes_library/common/parameters/diffusion/qwen/edit_parameters.py
+++ libraries/griptape_nodes_advanced_media_library/diffusers_nodes_library/common/parameters/diffusion/qwen/edit_parameters.py
@@ -7,7 +7,7 @@ import transformers  # type: ignore[reportMissingImports]
 from diffusers_nodes_library.common.parameters.diffusion.pipeline_type_parameters import (
     DiffusionPipelineTypePipelineParameters,
 )
-from diffusers_nodes_library.common.parameters.scheduler_parameters import SchedulerParameters
+from diffusers_nodes_library.common.parameters.diffusion.scheduler_parameters import SchedulerParameters
 from griptape_nodes.exe_types.node_types import BaseNode
 from griptape_nodes.exe_types.param_components.huggingface.huggingface_repo_parameter import HuggingFaceRepoParameter
 
