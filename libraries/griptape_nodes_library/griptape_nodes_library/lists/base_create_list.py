787ef5befa216cd295785c508df0d5b10abccc48
James Clarendon <james.clarendon@gmail.com>
2025-11-24 20:55:43 -0800

    Create a base class for lists to emit when they update

---
+++ libraries/griptape_nodes_library/griptape_nodes_library/lists/base_create_list.py
@@ -0,0 +1,59 @@
+from typing import Any
+
+from griptape_nodes.exe_types.core_types import (
+    Parameter,
+    ParameterList,
+    ParameterMode,
+)
+from griptape_nodes.exe_types.node_types import ControlNode
+
+
+class BaseCreateListNode(ControlNode):
+    """Base class for all create list nodes."""
+
+    def __init__(  # noqa: PLR0913
+        self,
+        name: str,
+        metadata: dict[Any, Any] | None,
+        *,
+        input_types: list[str],
+        output_type: str,
+        default_value: Any,
+        items_tooltip: str,
+    ) -> None:
+        super().__init__(name, metadata)
+
+        # Create items_list parameter
+        self.items_list = ParameterList(
+            name="items",
+            tooltip=items_tooltip,
+            input_types=input_types,
+            allowed_modes={ParameterMode.INPUT, ParameterMode.PROPERTY},
+            default_value=default_value,
+        )
+        self.add_parameter(self.items_list)
+
+        # Create output parameter
+        self.output = Parameter(
+            name="output",
+            tooltip="Output list",
+            output_type=output_type,
+            allowed_modes={ParameterMode.OUTPUT},
+        )
+        self.add_parameter(self.output)
+
+    def after_value_set(self, parameter: Parameter, value: Any) -> None:
+        # If items parameter or any child of items_list was set, update output immediately
+        if parameter.name.startswith(f"{self.items_list.name}_"):
+            self._update_output()
+        return super().after_value_set(parameter, value)
+
+    def process(self) -> None:
+        self._update_output()
+
+    def _update_output(self) -> None:
+        """Gets items, sets output value, and publishes update."""
+        list_values = self.get_parameter_value(self.items_list.name)
+        self.set_parameter_value(self.output.name, list_values)
+        self.parameter_output_values[self.output.name] = list_values
+        self.publish_update_to_parameter(self.output.name, list_values)
--- libraries/griptape_nodes_library/griptape_nodes_library/lists/create_list.py
+++ libraries/griptape_nodes_library/griptape_nodes_library/lists/create_list.py
@@ -1,63 +1,18 @@
 from typing import Any
 
-from griptape_nodes.exe_types.core_types import (
-    Parameter,
-    ParameterList,
-    ParameterMode,
-    ParameterTypeBuiltin,
-)
-from griptape_nodes.exe_types.node_types import ControlNode
+from griptape_nodes.exe_types.core_types import ParameterTypeBuiltin
+from griptape_nodes_library.lists.base_create_list import BaseCreateListNode
 
 
-class CreateList(ControlNode):
+class CreateList(BaseCreateListNode):
     """CreateList Node that that creates a list with items provided."""
 
     def __init__(self, name: str, metadata: dict[Any, Any] | None = None) -> None:
-        super().__init__(name, metadata)
-        # Add input list parameter
-        self.items_list = ParameterList(
-            name="items",
-            tooltip="List of items to add to",
+        super().__init__(
+            name,
+            metadata,
             input_types=[ParameterTypeBuiltin.ANY.value],
-            allowed_modes={ParameterMode.INPUT, ParameterMode.PROPERTY},
-        )
-        self.add_parameter(self.items_list)
-        self.output = Parameter(
-            name="output",
-            tooltip="Output list",
             output_type="list",
-            allowed_modes={ParameterMode.OUTPUT},
+            default_value=None,
+            items_tooltip="List of items to add to",
         )
-        self.add_parameter(self.output)
-
-    def set_parameter_value(
-        self,
-        param_name: str,
-        value: Any,
-        *,
-        initial_setup: bool = False,
-        emit_change: bool = True,
-        skip_before_value_set: bool = False,
-    ) -> None:
-        super().set_parameter_value(
-            param_name,
-            value,
-            initial_setup=initial_setup,
-            emit_change=emit_change,
-            skip_before_value_set=skip_before_value_set,
-        )
-
-        # If items parameter was set, update output immediately
-        if param_name == self.items_list.name:
-            self._update_output()
-
-    def process(self) -> None:
-        self._update_output()
-
-    def _update_output(self) -> None:
-        """Gets items, sets output value, and publishes update."""
-        list_values = self.get_parameter_value(self.items_list.name)
-        self.set_parameter_value(self.output.name, list_values)
-        self.parameter_output_values[self.output.name] = list_values
-
-        self.publish_update_to_parameter(self.output.name, list_values)
