5de754422b7546670b14831e672d8593dbb9fe20
Kate Forsberg <kate@griptape.ai>
2025-12-04 13:53:14 -0800

    bugfix to start dependencies

--- src/griptape_nodes/machines/control_flow.py
+++ src/griptape_nodes/machines/control_flow.py
@@ -473,6 +473,7 @@ class ControlFlowMachine(FSM[ControlFlowContext]):
                 node.state = NodeResolutionState.UNRESOLVED
                 # Use proxy node if this node is part of a group, otherwise use original node
                 node_to_add = node
+                disconnected = True
                 # Only add if not already added (proxy might already be in DAG)
                 if node_to_add.name not in dag_builder.node_to_reference:
                     # Now, we need to create the DAG, but it can't be queued or used until it's dependencies have been resolved.
@@ -482,9 +483,12 @@ class ControlFlowMachine(FSM[ControlFlowContext]):
                         correct_graph = flow_manager.is_node_connected(graph_start_node, node)
                         # This means this node is in the downstream connection of one of this graph.
                         if correct_graph:
+                            disconnected = False
                             if node.name not in dag_builder.start_node_candidates:
                                 dag_builder.start_node_candidates[node.name] = set()
                             dag_builder.start_node_candidates[node.name].add(graph_start_node_name)
+                    if disconnected:
+                        dag_builder.add_node_with_dependencies(node_to_add, node_to_add.name)
                 flow_manager.global_flow_queue.queue.remove(item)
 
         return start_nodes
