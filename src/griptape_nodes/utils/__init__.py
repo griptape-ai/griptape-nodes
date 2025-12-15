f3862be56a87406e68a1a7a87b5641b978c01195
Collin Dutter <collin@griptape.ai>
2025-12-15 13:13:53 -0800

    refactor: remove unneeded functions

--- src/griptape_nodes/utils/__init__.py
+++ src/griptape_nodes/utils/__init__.py
@@ -3,12 +3,11 @@
 from griptape_nodes.utils.async_utils import call_function
 from griptape_nodes.utils.http_file_patch import install_file_url_support
 from griptape_nodes.utils.path_utils import resolve_workspace_path
-from griptape_nodes.utils.url_utils import get_content_type_from_extension, validate_url
+from griptape_nodes.utils.url_utils import get_content_type_from_extension
 
 __all__ = [
     "call_function",
     "get_content_type_from_extension",
     "install_file_url_support",
     "resolve_workspace_path",
-    "validate_url",
 ]
