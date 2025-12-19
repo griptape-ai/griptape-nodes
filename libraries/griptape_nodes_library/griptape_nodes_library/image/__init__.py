27829e806eddc7eb9bea966ee3417123159d8d47
cjkindel <cjkindel@users.noreply.github.com>
2025-12-19 10:50:56 -0800

    remove init noise

--- libraries/griptape_nodes_library/griptape_nodes_library/image/__init__.py
+++ libraries/griptape_nodes_library/griptape_nodes_library/image/__init__.py
@@ -1,9 +1,1 @@
 """Image Nodes package."""
-
-from griptape_nodes_library.image.topaz_image_enhance import TopazImageEnhance
-from griptape_nodes_library.image.topaz_creative_enhance import TopazCreativeEnhance
-
-__all__ = [
-    "TopazImageEnhance",
-    "TopazCreativeEnhance",
-]
--- libraries/griptape_nodes_library/griptape_nodes_library/video/__init__.py
+++ libraries/griptape_nodes_library/griptape_nodes_library/video/__init__.py
@@ -1,7 +1,1 @@
 """Video nodes for loading, displaying, and saving video content."""
-
-from griptape_nodes_library.video.topaz_video_enhance import TopazVideoEnhance
-
-__all__ = [
-    "TopazVideoEnhance",
-]
