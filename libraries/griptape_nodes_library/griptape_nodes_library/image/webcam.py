16ec9dbffd6b34457ff1b8932b19f8fadc6cf27a
Jason Schleifer <jason.schleifer@gmail.com>
2026-01-21 15:16:48 +1300

    fix webcam

--- libraries/griptape_nodes_library/griptape_nodes_library/image/webcam.py
+++ libraries/griptape_nodes_library/griptape_nodes_library/image/webcam.py
@@ -1,6 +1,5 @@
-from griptape_nodes.exe_types.core_types import ParameterMode
+from griptape_nodes.exe_types.core_types import Parameter, ParameterMode
 from griptape_nodes.exe_types.node_types import DataNode
-from griptape_nodes.exe_types.param_types.parameter_image import ParameterImage
 from griptape_nodes_library.utils.image_utils import dict_to_image_url_artifact
 
 
@@ -8,10 +7,12 @@ class Webcam(DataNode):
     def __init__(self, **kwargs) -> None:
         super().__init__(**kwargs)
 
-        image_parameter = ParameterImage(
+        image_parameter = Parameter(
             name="image",
-            webcam_capture_image=True,
-            ui_options={"expander": True},
+            input_types=["ImageArtifact", "ImageUrlArtifact", "dict"],
+            type="ImageArtifact",
+            output_type="ImageUrlArtifact",
+            ui_options={"webcam_capture_image": True, "expander": True},
             tooltip="The image that has been captured.",
             allowed_modes={ParameterMode.OUTPUT, ParameterMode.PROPERTY},
         )
