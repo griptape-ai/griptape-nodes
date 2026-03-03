fc6a7c325ee42c673d807174ab5ab6b8ac2bfa04
matt <matt@griptape.ai>
2026-03-03 14:13:06 -0600

    move to app.py

--- src/griptape_nodes/__init__.py
+++ src/griptape_nodes/__init__.py
@@ -3,12 +3,7 @@
 import sys
 from pathlib import Path
 
-import truststore
 from rich.console import Console
-
-# uses the system certificate store for SSL verification
-# this patches the ssl module which is used by all HTTP libraries
-truststore.inject_into_ssl()
 
 
 def main() -> None:
